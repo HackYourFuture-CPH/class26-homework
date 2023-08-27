1bbfb4a352fd325f3c93752b752e12a2cc3e5671
hirenkumar91 <123118011+hirenkumar91@users.noreply.github.com>
2023-08-27 20:46:34 +0200

    dog age calculator done

--- 3_javascript/javascript1/week1/scritps.js
+++ 3_javascript/javascript1/week1/scritps.js
@@ -2,6 +2,25 @@
 
 let yearOfBirth = 1991;
 let yearFuture = 2027;
-let age = yearFuture-yearOfBirth;
+let age = yearFuture - yearOfBirth;
 console.log(age);
 console.log(`you will be ${age} years old in ${yearFuture}`);
+
+//Goodboy-Oldboy (A dog age calculator)
+
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let shouldShowResultInDogYears = true;
+
+let dogAgeInHumanYears = dogYearFuture - dogYearOfBirth;
+
+if (shouldShowResultInDogYears) {
+  let dogAgeInDogYears = dogAgeInHumanYears * 7; // Dog years calculation
+  console.log(
+    `Your dog will be ${dogAgeInDogYears} dog years old in ${dogYearFuture}`
+  );
+} else {
+  console.log(
+    `Your dog will be ${dogAgeInHumanYears} human years old in ${dogYearFuture}`
+  );
+}
