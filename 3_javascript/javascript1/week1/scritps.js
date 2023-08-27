162e0080e3c791c243b3d754d3fad19ef1b7a781
hirenkumar91 <123118011+hirenkumar91@users.noreply.github.com>
2023-08-27 22:26:32 +0200

    startup name generater made and some chnages made to house calculator

--- 3_javascript/javascript1/week1/scritps.js
+++ 3_javascript/javascript1/week1/scritps.js
@@ -3,7 +3,7 @@
 let yearOfBirth = 1991;
 let yearFuture = 2027;
 let age = yearFuture - yearOfBirth;
-console.log(age);
+
 console.log(`you will be ${age} years old in ${yearFuture}`);
 
 //Goodboy-Oldboy (A dog age calculator)
@@ -27,17 +27,67 @@ if (shouldShowResultInDogYears) {
 
 //Housey pricey (A house price estimator)
 
-let yourName = prompt("your Name");
-let width = prompt("Enter width");
-let depth = prompt("Enter depth");
-let hight = prompt("Enter hight");
-let gardenSizeInM2 = prompt("Enter garden size in M2");
-let houseCost = prompt("enter house cost");
-let volumeInMeters = width * depth * hight;
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+let friendsHouse = [
+  {
+    name: "Peter",
+    housePrice: 2500000,
+    width: 8,
+    depth: 10,
+    height: 10,
+    gardenSizeInM2: 300,
+  },
+  {
+    name: "Julia",
+    housePrice: 1000000,
+    width: 5,
+    depth: 11,
+    height: 8,
+    gardenSizeInM2: 70,
+  },
+];
 
-if (houseCost > housePrice) {
-  alert(`${yourName} is paying too much`);
+const Peterhousecost =
+  friendsHouse[0].width *
+    friendsHouse[0].height *
+    friendsHouse[0].depth *
+    2.5 *
+    1000 +
+  friendsHouse[0].gardenSizeInM2 * 300;
+
+const Juliahousecost =
+  friendsHouse[1].width *
+    friendsHouse[1].height *
+    friendsHouse[1].depth *
+    2.5 *
+    1000 +
+  friendsHouse[1].gardenSizeInM2 * 300;
+
+if (Peterhousecost > friendsHouse[0].housePrice) {
+  console.log(`${friendsHouse[0].name} is paying too much`);
 } else {
-  alert(`${yourName} paying too little`);
+  console.log(
+    `${friendsHouse[0].name} is paying ${Peterhousecost} which is reasonable`
+  );
 }
+
+if (Juliahousecost > friendsHouse[1].housePrice) {
+  console.log(
+    `${friendsHouse[1].name} is paying ${Juliahousecost} which is too much`
+  );
+} else {
+  console.log(
+    `${friendsHouse[1].name} is paying ${Juliahousecost} which is reasonable`
+  );
+}
+
+// Ez Namey (Startup name generator)
+
+let firstWords = ["Easy", "Awesome", "Corporate", "fun", "gun", "run"];
+let secondWords = ["ping", "pong", "Ding", "Dong", "stick", "apple", "banana"];
+const randomNumberone = Math.floor(Math.random() * 6);
+const randomNumbertwo = Math.floor(Math.random() * 7);
+let startupName = firstWords[randomNumberone] + secondWords[randomNumbertwo];
+
+console.log(
+  `"The startup: "${startupName}" contains ${startupName.length} characters"`
+);
