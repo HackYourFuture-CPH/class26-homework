bc4a8d3d7850d43a6cb1863b972cd879f0877d53
hirenkumar91 <123118011+hirenkumar91@users.noreply.github.com>
2023-08-27 20:34:16 +0200

    Age-ify (A future age calculator) made

---
+++ 3_javascript/javascript1/week1/scritps.js
@@ -0,0 +1,7 @@
+//Age-ify (A future age calculator)
+
+let yearOfBirth = 1991;
+let yearFuture = 2027;
+let age = yearFuture-yearOfBirth;
+console.log(age);
+console.log(`you will be ${age} years old in ${yearFuture}`);
