d3638fe44f0bc373730727803bc3c81549350d12
Sevda Naziri <sevdanaziri@gmail.com>
2023-08-31 12:50:23 +0200

    calculated the age by changing the year to 2027

--- 3_javascript/javascript1/week1/script.js
+++ 3_javascript/javascript1/week1/script.js
@@ -4,3 +4,11 @@ let yearFuture = 2045;
 // calculating the age
 let age = yearFuture - yearOfBirth;
 console.log(age);
+
+// changing the future year to 2027
+yearFuture = 2027;
+age = yearFuture - yearOfBirth;
+console.log(age);
+
+// log out using string
+console.log(`You will be ${age} in ${yearFuture}`);
