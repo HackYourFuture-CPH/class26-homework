d200e54b82731ab21584c21e05ecde47d074b8a4
Sevda Naziri <sevdanaziri@gmail.com>
2023-09-01 21:58:50 +0200

    added the dog age calculation

--- 3_javascript/javascript1/week1/script.js
+++ 3_javascript/javascript1/week1/script.js
@@ -1,4 +1,5 @@
-let yearOfBirth = 1989;
+// Age-ify
+let yearOfBirth = 1987;
 let yearFuture = 2045;
 
 // calculating the age
@@ -12,3 +13,20 @@ console.log(age);
 
 // log out using string
 console.log(`You will be ${age} in ${yearFuture}`);
+
+//A dog age calculator
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2045;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let dogString = "human";
+
+let shouldShowResultInDogYears = false;
+
+if (shouldShowResultInDogYears) {
+  dogYear *= 7;
+  dogString = "dog";
+}
+
+console.log(
+  `Your dog will be ${dogYear} ${dogString} years old in ${dogYearFuture}`
+);
