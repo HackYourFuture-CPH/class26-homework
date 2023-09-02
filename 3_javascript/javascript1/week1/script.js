abe2fb627f1d76bf3081fbd1ee8a1ef49ab48437
Sevda Naziri <sevdanaziri@gmail.com>
2023-09-02 13:35:20 +0200

    all the feedbacks implemented

--- 3_javascript/javascript1/week1/script.js
+++ 3_javascript/javascript1/week1/script.js
@@ -3,16 +3,15 @@ const yearOfBirth = 1987;
 let yearFuture = 2045;
 
 // calculating the age
-const age = yearFuture - yearOfBirth;
-console.log(age);
+let age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}`);
 
 // changing the future year to 2027
 yearFuture = 2027;
 age = yearFuture - yearOfBirth;
-console.log(age);
 
 // log out using string
-console.log(`You will be ${age} in ${yearFuture}`);
+console.log(`You will be ${age} years old in ${yearFuture}`);
 
 //A dog age calculator
 const dogYearOfBirth = 2017;
@@ -32,14 +31,14 @@ console.log(
 );
 
 //A house price estimator (Peter's house)
-let friendOne = "Peter";
 const peterHouse = {
+  friendOne: "Peter",
   width: 8,
   height: 10,
   depth: 10,
   gardenSize: 100,
+  housecost: 2500000,
 };
-const peterhousecost = 2500000;
 
 //calculating the volume
 let peterVolumeInMeters =
@@ -50,34 +49,35 @@ peterHousePrice =
   peterVolumeInMeters * 2.5 * 1000 + peterHouse.gardenSize * 300;
 
 //calculating Peter's house price difference
-let priceDifferencePeter = peterhousecost - peterHousePrice;
+let priceDifferencePeter = peterHouse.housecost - peterHousePrice;
 
 //calculating if Peter paid too much!
 console.log(
-  `${friendOne} will loose ${priceDifferencePeter} iif he buys the house`
+  `${peterHouse.friendOne} will loose ${priceDifferencePeter} if he buys the house`
 );
 
 //A house price estimator (Julia's house)
-let friendTwo = "Julia";
 const juliaHouse = {
+  friendTwo: "Julia",
   width: 5,
   height: 11,
   depth: 8,
   gardenSize: 70,
+  houseCost: 1000000,
 };
-const juliaHouseCost = 1000000;
 
 //calculating the volume
 let juliaHouseVolumeInMeters =
   juliaHouse.width * juliaHouse.height * juliaHouse.depth;
 
 //calculating actuall worth of Julia's house
-let juliaHousePrice = juliaHouseVolumeInMeters * 2.5 * 1000 + 70 * 300;
+let juliaHousePrice =
+  juliaHouseVolumeInMeters * 2.5 * 1000 + juliaHouse.gardenSize * 300;
 
 //calculating Julia's house price difference
-priceDifferenceJulia = juliaHousePrice - juliaHouseCost;
+priceDifferenceJulia = juliaHousePrice - juliaHouse.houseCost;
 
 //calculating if Julia paid too much!
 console.log(
-  `${friendTwo} will save ${priceDifferenceJulia} if she buys the house`
+  `${juliaHouse.friendTwo} will save ${priceDifferenceJulia} if she buys the house`
 );
