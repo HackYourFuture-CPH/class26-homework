ab4cc4e011acd21f085d76abf052d3e1862e12b6
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-03 10:41:46 +0100

    Goodboy-Oldboy part

--- 3_javascript/javascript1/week1/script.js
+++ 3_javascript/javascript1/week1/script.js
@@ -6,3 +6,21 @@ let age = yearFuture - yearOfBirth;
 let stringOutput = `You will be ${age} years old in ${yearFuture} .`
 // log age 
 console.log(stringOutput);
+
+
+// A dog age calculator
+let dogYearOfBirth = 2010;
+let dogYearFuture = 2027;
+
+let shouldShowResultInDogYears = true;
+let dogAgeStringOutput;
+
+if (shouldShowResultInDogYears) {
+    let dogAgeInDogYears = (dogYearFuture - dogYearOfBirth) * 7
+    dogAgeStringOutput = `Your dog will be ${dogAgeInDogYears} dog years old in ${dogYearFuture}`;
+  } else {
+    let dogAgeInHumanYears = dogYearFuture - dogYearOfBirth;
+    dogAgeStringOutput = `Your dog will be ${dogAgeInHumanYears} human years old in ${dogYearFuture}`;
+  }
+
+console.log(dogAgeStringOutput);
