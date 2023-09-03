9e26ba8c81d6891ed523ce224dd28165ac66cdf4
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-03 10:16:31 +0100

    homework part 1

---
+++ 3_javascript/javascript1/week1/script.js
@@ -0,0 +1,8 @@
+let yearOfBirth = 1998;
+let yearFuture = 2023;
+
+// calculat age 
+let age = yearFuture - yearOfBirth; 
+let stringOutput = `You will be ${age} years old in ${yearFuture} .`
+// log age 
+console.log(stringOutput);
