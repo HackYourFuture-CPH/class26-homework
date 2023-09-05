63e3d410256bf20584b1be6391552fda38e72302
Meraj Sharifi <merajsharifi@Merajs-MacBook-Pro.local>
2023-09-05 11:23:49 +0200

    creating file

---
+++ 3_javascript/javascript1/week2/js_w2.js
@@ -0,0 +1,12 @@
+
+function getFullname(firstname, surname) {
+    let fullname = firstname + " " + surname;
+    return fullname;
+  }
+  
+  let fullname1 = getFullname("John", "Doe");
+  let fullname2 = getFullname("Alice", "Smith");
+  
+  console.log("Full Name 1:", fullname1);
+  console.log("Full Name 2:", fullname2);
+  
