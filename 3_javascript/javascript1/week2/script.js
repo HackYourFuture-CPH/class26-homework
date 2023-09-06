e303429a7722e2bc1540af4d78e9ea05f2ee560a
hirenkumar91 <hmtrivedi.3@gmail.com>
2023-09-06 20:52:12 +0200

    fuction updated when female use this fuction

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -1,15 +1,31 @@
 /** Smart-ease - Goes Global! **/
 
-function getFullname(string , surName
-    , Boolean) {
-  le-length.t fullName = firstName + " " + surName;
-  if ((Boolean = true)) {
-    fullName = "lord " + firstName + " " + surName;
+function getFullname(Gender, firstName, surName, useFormalName) {
+  let fullName = "";
+
+  if (Gender === "female") {
+    if (useFormalName === true) {
+      fullName = `Lady ${firstName} ${surName}`;
+    } else {
+      fullName = `${firstName} ${surName}`;
+    }
+  } else {
+    if (useFormalName === true) {
+      fullName = `Lord ${firstName} ${surName}`;
+    } else {
+      fullName = `${firstName} ${surName}`;
+    }
   }
+
   return fullName;
 }
 
-const fullname1 = getFullname("banjamin", "Hughes", false);
-const fullname2 = getFullname("Banjamin", "Hughes");
+const fullname3 = getFullname("female", "Banjamin", "Hughes", false);
+const fullname4 = getFullname("female", "Banjamin", "Hughes", true);
+const fullname1 = getFullname("male", "Banjamin", "Hughes", false);
+const fullname2 = getFullname("male", "Banjamin", "Hughes", true);
 
 console.log(fullname1);
+console.log(fullname2);
+console.log(fullname3);
+console.log(fullname4);
