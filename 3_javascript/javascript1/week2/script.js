dd0f85490d16aea9b6b308cb4968f2c7351771bb
Sevda Naziri <sevdanaziri@gmail.com>
2023-09-06 21:47:41 +0200

    added the event aplication, will return to this since couldn't define only the day

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -14,3 +14,26 @@ let fullname2 = getFullname("John", "Wick", true, "man");
 
 console.log(fullname1);
 console.log(fullname2);
+
+//Event application
+let daysUntilEvent = 8;
+
+function getEventWeekday(days) {
+  var date = new Date();
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+console.log(getEventWeekday(daysUntilEvent));
+
+//Weather wear 
+let chillCloths = "You should wear T-shirt and shorts";
+let warmCloths = "You must wear warm cloths";
+
+function clothesToWear(tempreture) {
+  if(tempreture > 20) {
+    return ${chillCloths}
+  }
+
+}
+console.log(clothesToWear)
