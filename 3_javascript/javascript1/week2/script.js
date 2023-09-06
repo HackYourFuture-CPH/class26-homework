7bdbd89c2b050905e89d128d3ced165b40524091
Sevda Naziri <sevdanaziri@gmail.com>
2023-09-06 20:21:35 +0200

    finished the first part of the homework

---
+++ 3_javascript/javascript1/week2/script.js
@@ -0,0 +1,10 @@
+//Flight booking
+function getFullName(firstname, surname) {
+  return firstname + " " + surname;
+}
+
+let fullname1 = getFullName("Sevda", "Naziri");
+let fullname2 = getFullName("John", "Wick");
+
+console.log(fullname1);
+console.log(fullname2);
