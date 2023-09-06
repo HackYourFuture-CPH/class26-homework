b15e33bd02534c0ecbdb1f1824da893b2a11f759
Sevda Naziri <sevdanaziri@gmail.com>
2023-09-06 20:55:25 +0200

    finished Formal fullname

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -1,10 +1,16 @@
 //Flight booking
-function getFullName(firstname, surname) {
-  return firstname + " " + surname;
+function getFullname(firstname, surname, useFormalName, gender) {
+  if (useFormalName == true && gender == "man") {
+    return `Lord ${firstname} ${surname}`;
+  } else if (useFormalName == true && gender == "woman") {
+    return `Miss ${firstname} ${surname}`;
+  } else {
+    return `${firstname} ${surname}`;
+  }
 }
 
-let fullname1 = getFullName("Sevda", "Naziri");
-let fullname2 = getFullName("John", "Wick");
+let fullname1 = getFullname("Sevda", "Naziri", true, "woman");
+let fullname2 = getFullname("John", "Wick", true, "man");
 
 console.log(fullname1);
 console.log(fullname2);
