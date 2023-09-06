b57f58dacf8be3bf44aa347fc1253b96f5d7e6f4
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-06 13:52:27 +0100

    Housey pricey

--- 3_javascript/javascript1/week1/script.js
+++ 3_javascript/javascript1/week1/script.js
@@ -24,3 +24,40 @@ if (shouldShowResultInDogYears) {
   }
 
 console.log(dogAgeStringOutput);
+// House price 
+
+let gardenSizeInM2 = 100;
+function housePrice(volumeInMeters,gardenSizeInM2){
+  return  volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+}
+//peter's house
+let pHeight = 10;
+let pWidth = 8;
+let PDepth = 10;
+let pVolumeInMeters = pHeight * pWidth * PDepth;
+let pGardenSize = 100;
+let peterHasPayed = 2500000;
+let  peterHousePrice = housePrice(pVolumeInMeters,pGardenSize)
+if (peterHasPayed < peterHousePrice ) {
+  console.log(`Peter has payed ${peterHasPayed} too little for the house ${peterHousePrice}.`);
+} else if (peterHasPayed > peterHousePrice) {
+  console.log(`Peter has payed ${peterHasPayed} too much for the house  ${peterHousePrice}.` );
+} else {
+  console.log(`Peter has payed ${peterHasPayed} the expected price for the house  ${peterHousePrice}.` );
+}
+
+// julis's house
+let jHheight = 8;
+let jWidth = 5;
+let jDepth = 11;
+let jVolumeInMeters = jHheight * jWidth * jDepth;
+let jGardenSize = 70;
+let juiliaHasPayed = 1000000;
+let  juiliaHousePrice = housePrice(jVolumeInMeters,jGardenSize)
+if (juiliaHasPayed < juiliaHousePrice ) {
+  console.log(`juilia has payed ${juiliaHasPayed} too little for the house ${juiliaHousePrice}.`);
+} else if (juiliaHasPayed > juiliaHousePrice) {
+  console.log(`juilia has payed ${juiliaHasPayed} too much for the house  ${juiliaHousePrice}.` );
+} else {
+  console.log(`juilia has payed ${juiliaHasPayed} the expected price for the house  ${juiliaHousePrice}.` );
+}
