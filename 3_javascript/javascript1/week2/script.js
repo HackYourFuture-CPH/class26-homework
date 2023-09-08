893a1d351c08abc39b283b1fa5689d0810c21916
ayaroshenkocom <mailfortools@gmail.com>
2023-09-08 18:53:35 +0200

    Candy helper optional

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -25,6 +25,8 @@ console.log(fullname4);
 
 
 
+
+
 // Event application
 function getEventWeekday(daysUntilEvent) {
     const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -38,6 +40,8 @@ function getEventWeekday(daysUntilEvent) {
 
 console.log("The event will take place on " + getEventWeekday(3));
 console.log("The event will take place on " + getEventWeekday(7));
+
+
 
 
 
@@ -56,6 +60,7 @@ function recommendClothes(temperature) {
 
 const clothesToWear = recommendClothes(19);
 console.log(clothesToWear);
+
 
 
 
@@ -88,3 +93,60 @@ addStudentToClass(""); // Empty string
 console.log(getNumberOfStudents()); //Display the number of students
 
 
+
+
+// Candy helper optional
+const boughtCandyPrices = []; // Array for storing prices for candies
+
+function addCandy(candyType, weight) {
+    let pricePerGram = 0; // Cost of candy per gram
+
+    // Determine the cost of candies depending on the type
+    switch (candyType) {
+        case "sweet":
+            pricePerGram = 0.5;
+            break;
+        case "chocolate":
+            pricePerGram = 0.7;
+            break;
+        case "toffee":
+            pricePerGram = 1.1;
+            break;
+        case "chewing-gum":
+            pricePerGram = 0.03;
+            break;
+        default:
+            console.log("Invalid candy type");
+            return; // Exit the function if the candy type is incorrect
+    }
+
+    const candyPrice = pricePerGram * weight; // Calculating the cost of sweets
+    boughtCandyPrices.push(candyPrice); // Adding cost to the array
+}
+
+
+const amountToSpend = Math.random() * 100; ////the number will represent the amount you can spend on candy
+function canBuyMoreCandy() {
+    let totalCandyPrice = 0;
+
+    // We calculate the total cost of all candies purchased
+    for (const candyPrice of boughtCandyPrices) {
+        totalCandyPrice += candyPrice;
+    }
+    // Compare the total cost with the available funds
+    if (totalCandyPrice <= amountToSpend) {
+        return true; // You can buy more candy
+    } else {
+        return false; // Enough candy for you
+    }
+}
+
+addCandy("sweet", 20); // Add 20 grams of sweets
+addCandy("chocolate", 30); // Add 30 grams of chocolate
+// Add more candy if you like
+
+if (canBuyMoreCandy()) {
+    console.log("You can buy more, so please do!");
+} else {
+    console.log("Enough candy for you!");
+}
