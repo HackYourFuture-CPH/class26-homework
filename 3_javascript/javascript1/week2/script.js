8206c6cbc308a1b9ba47ae59155f8d6aab2b7549
ayaroshenkocom <mailfortools@gmail.com>
2023-09-08 19:18:46 +0200

    Event application

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -22,3 +22,21 @@ console.log(fullname1);
 console.log(fullname2);
 console.log(fullname3);
 console.log(fullname4);
+
+
+
+
+
+// Event application
+function getEventWeekday(daysUntilEvent) {
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const today = new Date().getDay(); //Get today's day
+    const eventDayIndex = (today + daysUntilEvent) % 7; // Calculate the index of the day of week of the event
+
+    const eventDay = weekdays[eventDayIndex]; // Get of the day of the event
+    return eventDay;
+}
+
+console.log("The event will take place on " + getEventWeekday(3));
+console.log("The event will take place on " + getEventWeekday(7));
