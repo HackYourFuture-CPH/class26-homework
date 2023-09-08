e1e75830e30b597b68c54c3f12fc33cceee6de68
ayaroshenkocom <mailfortools@gmail.com>
2023-09-08 18:34:31 +0200

    Event application

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -17,6 +17,27 @@ function getFullname(firstname, surname, useFormalName, isFemale) {
 
 let fullname1 = getFullname("Benjamin", "Hughes", true, false);
 let fullname2 = getFullname("BenjaminA", "Button", true, true);
-
+let fullname3 = getFullname("BenjaminA", "Button", false, false);
+let fullname4 = getFullname("", "Button", true, true);
 console.log(fullname1);
 console.log(fullname2);
+console.log(fullname3);
+console.log(fullname4);
+
+
+
+// Event application
+
+function getEventWeekday(daysUntilEvent) {
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const today = new Date().getDay(); //Get today's day
+    const eventDayIndex = (today + daysUntilEvent) % 7; // Calculate the index of the day of week of the event
+
+    const eventDay = weekdays[eventDayIndex]; // Get of the day of the event
+    return eventDay;
+}
+
+console.log("The event will take place on " + getEventWeekday(3));
+console.log("The event will take place on " + getEventWeekday(7));
+
