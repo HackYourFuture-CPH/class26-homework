e4c2dfa462ba42ec6eecdb2f7c06ab2e575217ff
hirenkumar91 <hmtrivedi.3@gmail.com>
2023-09-09 23:01:11 +0200

    adding student name list

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -25,6 +25,11 @@ const fullname4 = getFullname("female", "Banjamin", "Hughes", true);
 const fullname1 = getFullname("male", "Banjamin", "Hughes", false);
 const fullname2 = getFullname("male", "Banjamin", "Hughes", true);
 
+console.log(fullname2);
+console.log(fullname1);
+console.log(fullname3);
+console.log(fullname4);
+
 /** Event application **/
 
 function getEventWeekday(daysFromToday) {
@@ -43,8 +48,9 @@ function getEventWeekday(daysFromToday) {
   return days[futureDate.getDate()];
 }
 
-console.log(getEventWeekday(9));
-console.log(getEventWeekday(2));
+const getEventday = getEventWeekday(9);
+
+console.log(getEventday);
 
 /** Weather wear */
 
@@ -77,3 +83,47 @@ function clothingChoice(temperature, humidity, windSpeed) {
     return "It's freezing. Bundle up with a heavy coat, scarf, gloves, and warm clothing.";
   }
 }
+
+const temperature = 25;
+const humidity = 45;
+const windSpeed = 10;
+
+const clothingAdvice = clothingChoice(temperature, humidity, windSpeed);
+console.log(clothingAdvice);
+
+const class07Students = [];
+
+function addStudentToClass(studentName) {
+  if (studentName === "") {
+    console.log("Cannot add an empty string to the class.");
+  } else if (class07Students.includes(studentName)) {
+    console.log(`Student ${studentName} is already in the class.`);
+  } else if (
+    class07Students.length >= 6 &&
+    studentName.toLowerCase() !== "queen"
+  ) {
+    console.log("Cannot add more students to class 07.");
+  } else {
+    class07Students.push(studentName);
+  }
+}
+
+function getNumberOfStudents() {
+  return class07Students.length;
+}
+
+addStudentToClass("Alice");
+addStudentToClass("Bob");
+addStudentToClass("Carol");
+addStudentToClass("Dave");
+addStudentToClass("Eve");
+addStudentToClass("Frank");
+addStudentToClass("Grace");
+addStudentToClass("Bob");
+addStudentToClass("Queen");
+
+// Log the number of students in the class
+console.log("Number of students in class 07:", getNumberOfStudents());
+
+// Print the current class list
+console.log("Current class list:", class07Students);
