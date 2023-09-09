c763e3b9c6b039e54f2341cdf4937b01d2b5051e
Sevda Naziri <sevdanaziri@gmail.com>
2023-09-09 20:54:43 +0200

    implemented the changes

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -1,16 +1,16 @@
 //Flight booking
 function getFullname(firstname, surname, useFormalName, gender) {
-  if (useFormalName == true && gender == "man") {
+  if (useFormalName === true && gender === "man") {
     return `Lord ${firstname} ${surname}`;
-  } else if (useFormalName == true && gender == "woman") {
+  } else if (useFormalName === true && gender === "woman") {
     return `Miss ${firstname} ${surname}`;
   } else {
     return `${firstname} ${surname}`;
   }
 }
 
-let fullname1 = getFullname("Sevda", "Naziri", true, "woman");
-let fullname2 = getFullname("John", "Wick", true, "man");
+const fullname1 = getFullname("Sevda", "Naziri", true, "woman");
+const fullname2 = getFullname("John", "Wick", true, "man");
 
 console.log(fullname1);
 console.log(fullname2);
@@ -19,31 +19,44 @@ console.log(fullname2);
 let daysUntilEvent = 8;
 
 function getEventWeekday(days) {
-  var date = new Date();
-  date.setDate(date.getDate() + days);
-  return date;
+  const dayOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date();
+  let currentDay = d.getDay();
+  let eventDay = (currentDay + days) % 7;
+  return dayOfTheWeek[eventDay];
 }
+
 console.log(getEventWeekday(daysUntilEvent));
 
 //Weather wear
 
 function whatShouldIWear(tempreture) {
   if (tempreture >= 20) {
-    return `You should wear T-shirt and shorts}`;
+    return "You should wear T-shirt and shorts";
   } else if (tempreture < 20) {
-    return `You must wear warm cloths`;
+    return "You must wear warm cloths";
   }
 }
-console.log(whatShouldIWear(20));
+console.log(whatShouldIWear(22));
 
 //Student manager
 
 const class07Students = [];
 function addStudentToClass(studentName) {
   if (class07Students.length >= 6 && studentName !== "Queen") {
-    return `Cannot add more students to class 07 - Queen always can join the class`;
+    console.log(
+      "Cannot add more students to class 07 - Queen always can join the class"
+    );
   } else if (class07Students.includes(studentName)) {
-    return `Student Benjamin is already in the class`;
+    console.log("Student Benjamin is already in the class");
   } else if (studentName === "") {
     console.log("Cannot add an empty string to a class");
   } else {
