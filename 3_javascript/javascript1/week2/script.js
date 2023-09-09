9bc8e3f0b5aef4edca00755bfb9403275e43a7e0
hirenkumar91 <hmtrivedi.3@gmail.com>
2023-09-09 22:43:11 +0200

    cloth choice function

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -45,3 +45,35 @@ function getEventWeekday(daysFromToday) {
 
 console.log(getEventWeekday(9));
 console.log(getEventWeekday(2));
+
+/** Weather wear */
+
+function clothingChoice(temperature, humidity, windSpeed) {
+  if (temperature >= 30) {
+    if (humidity >= 60) {
+      return "It's hot and humid. Wear light, breathable clothing and use sunscreen.";
+    } else {
+      return "It's hot. Wear light clothing and sunscreen.";
+    }
+  } else if (temperature >= 20 && temperature < 30) {
+    if (humidity >= 70) {
+      return "It's warm and humid. A t-shirt and shorts will be comfortable.";
+    } else {
+      return "It's warm. Wear a t-shirt and shorts.";
+    }
+  } else if (temperature >= 10 && temperature < 20) {
+    if (windSpeed >= 15) {
+      return "It's cooler and windy. A light jacket or sweater will help you stay warm.";
+    } else {
+      return "It's cooler. A light jacket or sweater will keep you comfortable.";
+    }
+  } else if (temperature >= 0 && temperature < 10) {
+    if (windSpeed >= 25) {
+      return "It's cold and windy. Wear a warm jacket, gloves, and jeans.";
+    } else {
+      return "It's cold. A warm jacket, jeans, and gloves will keep you warm.";
+    }
+  } else {
+    return "It's freezing. Bundle up with a heavy coat, scarf, gloves, and warm clothing.";
+  }
+}
