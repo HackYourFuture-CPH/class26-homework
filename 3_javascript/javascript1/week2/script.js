b725cc5bbe70e6cb8675a42624435689f6f2cdc0
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-09 14:17:06 +0100

    fullname methode

---
+++ 3_javascript/javascript1/week2/script.js
@@ -0,0 +1,7 @@
+
+// exercice full_name
+function getFullname(firstname, surname){
+    return firstname + " " + surname;
+}
+const fullName = getFullname("Benjamin", "Hughes");
+console.log(fullName);
