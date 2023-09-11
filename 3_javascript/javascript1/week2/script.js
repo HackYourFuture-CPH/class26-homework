3cc1dbcdca479911ec24e9779375bfd3e9193604
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-11 13:06:30 +0100

    exercice 1 and 2

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -22,3 +22,29 @@ let fullName1 = getFullname("nada", "jrad") ;
 let fullName2 = getFullname("jannah", "jrad") ;
 console.log(fullName1);
 console.log(fullName2);
+
+// event day 
+function getEventWeekday(numberOfTheDay){
+    let today = new Date();
+    let day = today.getDay();//1,2,3...
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday" , "Thursday", "Friday", "Saturday"];
+   
+    return weekdays[(day + numberOfTheDay)];
+}
+
+console.log(getEventWeekday(0));
+console.log(getEventWeekday(3));
+console.log(getEventWeekday(9));
+
+// weather
+function weather(tempertaure){
+    if(tempertaure >= 20 ){
+        return "wear shorts and dress";
+    }  if (tempertaure <= 10 ){
+        return "wear  a coat ";
+    } else {
+        return "wear jacket jeans";
+    }
+}
+let  clothesToWear = weather(8);
+console.log(clothesToWear);
