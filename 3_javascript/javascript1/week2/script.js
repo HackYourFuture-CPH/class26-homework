7e49b4dadce5359c58460b851cbb5aae4ee9e53e
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-11 09:32:47 +0100

    Flight booking fullname function

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -1,7 +1,24 @@
 
 // exercice full_name
-function getFullname(firstname, surname){
-    return firstname + " " + surname;
+function getFullname(firstname, surname, useFormalName, isWomen ){
+
+    if( firstname === "" & surname === ""){
+        return "firstname and surname are missing"
+    }
+    if (useFormalName === true){
+        if (isWomen === true){
+            return `lady ${firstname} ${surname}`;
+        } else{
+            return `Lord ${firstname} ${surname}`;
+        }    
+    } else {
+        return `${firstname} ${surname}`;
+    }
 }
-const fullName = getFullname("Benjamin", "Hughes");
+
+const fullName = getFullname("Benjamin", "Hughes",true);
 console.log(fullName);
+let fullName1 = getFullname("nada", "jrad") ;
+let fullName2 = getFullname("jannah", "jrad") ;
+console.log(fullName1);
+console.log(fullName2);
