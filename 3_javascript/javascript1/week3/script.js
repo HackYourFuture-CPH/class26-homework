ff79913d8030deadb101dee5b318f384dd946495
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-15 15:25:17 +0100

    When will we be there??

--- 3_javascript/javascript1/week3/script.js
+++ 3_javascript/javascript1/week3/script.js
@@ -1,3 +1,4 @@
+// Item array removal
 const names = [
     "Peter",
     "Ahmad",
@@ -14,8 +15,29 @@ if (names[i] == "Ahmad"){
     names.splice(i,1);
 }
 }
-  // Write some code here
-  
   // Code done
   
   console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
+  //When will we be there??
+ 
+  const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+  }
+
+  function calculateTravelTime(travelInformation){
+    let totaleTime = travelInformation.destinationDistance / travelInformation.speed 
+    let totalHours = Math.floor(totaleTime);
+
+
+    let minutes = (totaleTime - totalHours) * 60;
+
+    let totalMinutes = Math.round(minutes);
+
+    return `${totalHours} hours and ${totalMinutes} minutes`;
+  }
+
+  const travelTime = calculateTravelTime(travelInformation);
+  console.log(travelTime);
+  // code done
