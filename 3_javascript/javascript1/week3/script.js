d76242dae03fb81dad8c850bc5ba7c72e3bf6101
ayaroshenkocom <mailfortools@gmail.com>
2023-09-16 13:31:35 +0200

    Series duration of my life

--- 3_javascript/javascript1/week3/script.js
+++ 3_javascript/javascript1/week3/script.js
@@ -50,3 +50,58 @@ console.log(travelTime); // 8 hours and 38 minutes
 
 
 
+
+// Series duration of my life
+
+const seriesDurations = [
+    {
+        title: "Game of thrones",
+        days: 3,
+        hours: 1,
+        minutes: 0,
+    },
+    {
+        title: "Sopranos",
+        days: 3,
+        hours: 14,
+        minutes: 0,
+    },
+    {
+        title: "The Wire",
+        days: 2,
+        hours: 12,
+        minutes: 0,
+    },
+];
+
+seriesDurations.push({
+    title: "Breaking Bad",
+    days: 2,
+    hours: 14,
+    minutes: 0,
+});
+
+function logOutSeriesText() {
+    let totalPercentage = 0;
+
+    for (let i = 0; i < seriesDurations.length; i++) {
+        const series = seriesDurations[i];
+        const daysInMinutes = series.days * 24 * 60;
+        const hoursInMinutes = series.hours * 60;
+        const totalMinutes = daysInMinutes + hoursInMinutes + series.minutes;
+        const totalYears = totalMinutes / (60 * 24 * 365);
+        const percentageOfLife = (totalYears / 80) * 100;
+
+        console.log(`${series.title} took ${percentageOfLife.toFixed(3)}% of my life`);
+
+        totalPercentage += percentageOfLife;
+    }
+
+    console.log(`In total that is ${totalPercentage.toFixed(3)}% of my life`);
+}
+
+logOutSeriesText();
+
+
+
+
