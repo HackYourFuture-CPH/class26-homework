b9ab483a79af4d99128cea6cfc1d8b26dd801536
hirenkumar91 <hmtrivedi.3@gmail.com>
2023-09-16 23:31:35 +0200

    when will we be there ? done

--- 3_javascript/javascript1/week3/script.js
+++ 3_javascript/javascript1/week3/script.js
@@ -16,3 +16,28 @@ names.splice(indexOfnameToRemove, 1);
 // Code done
 
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
+/**When will we be there?? */
+
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function travelTimecalculation(travelInformation) {
+  const { speed, destinationDistance } = travelInformation;
+
+  // time calculation
+  const timeInhours = destinationDistance / speed;
+
+  const hours = Math.floor(timeInhours);
+  const minutes = Math.round((timeInhours - hours) * 60);
+
+  const timeFormatt = `${hours} hours and ${minutes} minutes`;
+
+  return timeFormatt;
+}
+
+const travelTime = travelTimecalculation(travelInformation);
+
+console.log(travelTime);
