b0a7d8d44eff36d1e42d2fee4f9a6dfea487882e
ayaroshenkocom <mailfortools@gmail.com>
2023-09-21 21:55:28 +0200

    Restaurant Order Management

--- 3_javascript/javascript1/week4/script.js
+++ 3_javascript/javascript1/week4/script.js
@@ -57,3 +57,74 @@ searchBook('Physics for Engineers');  // Expected: Sorry, Physics for Engineers 
 
 
 
+
+// --> Exercise 5: Restaurant Order Management <--
+
+let restaurantMenu = [
+    { dishName: "Spaghetti Carbonara", price: 12, isAvailable: true },
+    { dishName: "Chicken Parmesan", price: 15, isAvailable: false },
+    { dishName: "Hamburger", price: 25, isAvailable: true },
+    { dishName: "Durum Shawerma", price: 10, isAvailable: true },
+];
+
+// --> Order a Dish
+
+function orderDish(dishName) {
+    for (let i = 0; i < restaurantMenu.length; i++) {
+        if (restaurantMenu[i].dishName === dishName) {
+            if (restaurantMenu[i].isAvailable) {
+                console.log(`Your order for ${dishName} has been placed!`);
+            } else {
+                console.log(`We're sorry, ${dishName} is not available today.`);
+            }
+        }
+    } return
+}
+
+orderDish("Spaghetti Carbonara")
+orderDish("Chicken Parmesan")
+orderDish("Hamburger")
+
+// --> Check the Price
+
+function checkPrice(dishName) {
+    for (let c = 0; c < restaurantMenu.length; c++) {
+        if (restaurantMenu[c].dishName === dishName) {
+            console.log(`Your bill for ${dishName} is $${restaurantMenu[c].price}.`)
+            return
+        }
+    } console.log(`Sorry, we don't have ${dishName} on our menu.`)
+}
+
+checkPrice("Spaghetti Carbonara")
+checkPrice("Hamburger")
+checkPrice("Hamburger2")
+
+// --> Daily Special
+
+function dailySpecial() {
+    console.log(`Today's special is ${dishName} for only $${price}!`)
+}
+
+function dailySpecial() {
+    const availableDishes = restaurantMenu.filter(dish => dish.isAvailable); // Creating an array of available dishes
+
+    if (availableDishes.length === 0) {
+        console.log("Sorry, there are no available dishes for today's special.");
+        return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * availableDishes.length); // Generating a random index from available dishes
+    const specialDish = availableDishes[randomIndex];
+
+    console.log(`Today's special is ${specialDish.dishName} for only $${specialDish.price}!`);
+}
+
+
+dailySpecial();
+
+
+
+
+
+
