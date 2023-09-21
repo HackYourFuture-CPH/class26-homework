9228cca6e171da13cd4c7d52eef4795355723f02
270516 <ranjankomal93@gmail.com>
2023-09-21 21:19:41 +0200

    Implemented feedback

--- 3_javascript/javascript1/week1/script.js
+++ 3_javascript/javascript1/week1/script.js
@@ -1,22 +1,22 @@
 
 //A future age calculator
-let yearOfBirth = 1993 ;
+const yearOfBirth = 1993 ;
 
-let yearFuture = 2030 ;
+const yearFuture = 2030 ;
 
 const age = yearFuture - yearOfBirth ;
 
-const theSentence = "You will be" + " " + age + " " +  "years old in" + " " + yearFuture+ "." ;
+const theSentence = `${"You will be " + age + "years old in " + yearFuture}`;
 
 console.log(theSentence);
 
 //A dog age calculator
 
-let dogYearOfBirth = 2020 ;
+const dogYearOfBirth = 2020 ;
 
-let dogYearFuture = 2030 ;
+const dogYearFuture = 2030 ;
 
-let shouldShowResultInDogYears = true ;
+const shouldShowResultInDogYears = true ;
 
 const dogAgeInHumanYear = dogYearFuture - dogYearOfBirth ;
 
@@ -34,7 +34,7 @@ console.log(dogAge);
 
 //A house price estimator
 
-function calculateHousePrice(volumeInMeters,gardenSizeInM2 ) {
+function calculateHousePrice (volumeInMeters, gardenSizeInM2) {
 return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 }
 
@@ -54,39 +54,49 @@ let juliaHouse = {
     price : 1000000
 };
 
-let peterHouseVolume = peterHouse.width * peterHouse.depth * peterHouse.height ;
-let juliaHouseVolume = juliaHouse.width * juliaHouse.depth * juliaHouse.height ;
+const peterHouseVolume = peterHouse.width * peterHouse.depth * peterHouse.height ;
+const juliaHouseVolume = juliaHouse.width * juliaHouse.depth * juliaHouse.height ;
 
-let peterExpectedPrice = calculateHousePrice(peterHouseVolume, peterHouse.garden);
-let juliaExpectedPrice = calculateHousePrice(juliaHouseVolume, juliaHouse.garden);
+const peterExpectedPrice = calculateHousePrice(peterHouseVolume, peterHouse.garden);
+const juliaExpectedPrice = calculateHousePrice(juliaHouseVolume, juliaHouse.garden);
 
 if (peterHouse.price > peterExpectedPrice) {
- console.log("Peter is paying too low.");
+ 
+    console.log("Peter is paying too low.");
+
 } else if (peterHouse.price < peterExpectedPrice){
+
     console.log("Peter is paying too high");
+
 } else {
+
     console.log("Peter is paying accurate and correct" )
 }
 
 if (juliaHouse.price > juliaExpectedPrice) {
+    
     console.log("Julia is paying too low.");
-   } else if (juliaHouse.price < juliaExpectedPrice){
-       console.log("Julia is paying too high");
-   } else {
-       console.log("Julia is paying accurate and correct" )
+   
+} else if (juliaHouse.price < juliaExpectedPrice){
+
+    console.log("Julia is paying too high");
+
+} else {
+
+    console.log("Julia is paying accurate and correct" )
    }
    
 
 //Startup name generator
 
-let firstWords = ["Easy", "Awesome", "Corporate","Funny", "Lazy", "Dreamy", "Koool", "Drool", "Wheely", "Lily"];
-let secondWords = ["Chaipoint", "Funny", "Klassy", "Narannaran", "Kurkure", "Lays", "Smokey", "Chilly", "Kabutar", "TamTam"];
+const firstWords = ["Easy", "Awesome", "Corporate","Funny", "Lazy", "Dreamy", "Koool", "Drool", "Wheely", "Lily"];
+const secondWords = ["Chaipoint", "Funny", "Klassy", "Narannaran", "Kurkure", "Lays", "Smokey", "Chilly", "Kabutar", "TamTam"];
 
-const randomNumber = Math.floor(Math.random() * 10);
+const randomNumber = Math.floor(Math.random() * firstWords.length);
 
-let startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
+const startupName = `${firstWords[randomNumber]} ${secondWords[randomNumber]}`;
 
-let nameLength = startupName.length;
+const nameLength = startupName.length;
 
-console.log("The startup:" + " " + startupName + " " + "contains" + " " + nameLength + " " + "characters");
+console.log(`The startup: ${startupName} contains ${nameLength} characters`);
 
