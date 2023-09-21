35055e40660da2c36ce4c09986aa172176123933
shazkhan2 <seewardak@hotmail.com>
2023-09-21 13:23:27 +0200

    3 more to go

--- 3_javascript/javascript1/week4/script4.js
+++ 3_javascript/javascript1/week4/script4.js
@@ -30,8 +30,20 @@ return `${activity} added to your todo`;
 function activityToRemove(activity) {
     let remainingActivities = aboutMe.filter(todo => todo !== activity);
     aboutMe = remainingActivities;
-    return remainingActivities;
+    return `removed ${activity} from your todo list`;
 }
+// what is on my todo
+
+function viewToDo() {
+    const excludedNames = ["Benjamin", "Ben"];
+    const activities = aboutMe.filter(activity => !excludedNames.includes(activity));
+    if (activities.length > 0) {
+        console.log(`Your todo list: ${activities.join(", ")}`);
+    } else {
+        console.log("Your todo list is empty");
+    }
+}
+
 
 
 console.log(getReply("Hello my name is Benjamin")); // Output: "Nice to meet you Benjamin"
@@ -42,6 +54,5 @@ console.log(aboutMe); // Output: ["Benjamin"]
 console.log(toDoList("fishing")); //fishing added to your todo
 console.log(toDoList("singing in the shower")); //singing in the shower added to your todo
 console.log(aboutMe); // Output: ['Benjamin', 'Ben', 'fishing', 'swimming']
-console.log(activityToRemove("fishing")); // ['Benjamin', 'Ben', 'singing in the shower']
-
-
+console.log(activityToRemove("fishing")); // [removed fishing from your todo list]
+console.log(viewToDo());
