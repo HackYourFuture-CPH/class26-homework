f5738e6fb1307d64c9b8a474993d254a915900ad
shazkhan2 <seewardak@hotmail.com>
2023-09-22 12:14:55 +0200

    Final task done

--- 3_javascript/javascript1/week4/script4.js
+++ 3_javascript/javascript1/week4/script4.js
@@ -38,21 +38,60 @@ function viewToDo() {
     const excludedNames = ["Benjamin", "Ben"];
     const activities = aboutMe.filter(activity => !excludedNames.includes(activity));
     if (activities.length > 0) {
-        console.log(`Your todo list: ${activities.join(", ")}`);
+        console.log(`Your todo list is: ${activities.join(", ")}`);
     } else {
         console.log("Your todo list is empty");
     }
 }
 
+// What date is it today
+const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+const now = new Date();
+const day = now.getDate(); 
+const month = months[now.getMonth()];
+const year = now.getFullYear();
+
+// Should be able to do simple math. fx what is 3 + 3 should respond with 6. Or what is 4 * 12 should respond with 48
+
+function calculate (num1, num2, operation) {
+
+if (operation ==="add") {
+    return num1 + num2;
+
+} else if (operation === "multiply") {
+    return num1 * num2;
+
+} else {
+    return "Operation not supported";
+
+}
+}
+let result = calculate (3,3,"add");
+let result1 = calculate (4,12,"multiply");
+let result2 = calculate (9,3,"Nothing");
+
+// Set a timer for 4 minutes
+setTimeout(myTimeout, 4000);
+
+function myTimeout() {
+    document.getElementById("demo").innerHTML = "I can do magic, woooooo!";
+}
 
 
 console.log(getReply("Hello my name is Benjamin")); // Output: "Nice to meet you Benjamin"
 console.log(getReply("What is my name?")); // Output: "I'm sorry, I don't understand that command."
 console.log(getReply("Hello my name is Ben")); // Output: "Nice to meet you Ben"
 console.log(getReply("Hello my name is Benjamin")); // Output: "You have already said that!"
-console.log(aboutMe); // Output: ["Benjamin"]
+console.log(aboutMe); // Output: ["Benjamin, Ben"]
 console.log(toDoList("fishing")); //fishing added to your todo
 console.log(toDoList("singing in the shower")); //singing in the shower added to your todo
 console.log(aboutMe); // Output: ['Benjamin', 'Ben', 'fishing', 'swimming']
 console.log(activityToRemove("fishing")); // [removed fishing from your todo list]
-console.log(viewToDo());
+viewToDo(); //It only shows one output- "singing in the shower" as I had deleted the first activity in the previous task.
+console.log(`Today is ${day}. of ${month} ${year}`); //It works without a function. DO I still have to create a function?
+console.log (`Your answer is ${result}`);
+console.log (`Your answer is ${result1}`);
+console.log (`${result2}`);
