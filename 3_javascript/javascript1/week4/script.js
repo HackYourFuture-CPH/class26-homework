d42bb95ec89f0b3c51701bfb8bc564e4685d48bb
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-23 16:02:12 +0100

    Add/removed task

--- 3_javascript/javascript1/week4/script.js
+++ 3_javascript/javascript1/week4/script.js
@@ -1,35 +1,61 @@
 
 
-const User ={
-    userName:"",
-    todos:[],
+const user = {
+    userName: "",
+    todos: []
+}
 
-} 
 function getReply(command) {
-    userName = extractName(command)
+
     //command format: Hello my Name is Name
     if (command.match("Hello my Name is ")) {
-        return `Nice to meet you ${User.userName}!`;
-    }  if (command.match ("What is my name?")) {
-        return `Your name is ${User.userName}`;
+        user.userName = extractName(command)
+        return `Nice to meet you ${user.userName}!`;
     }
-    if(command.match("Add fishing to my todo")){
-        let taskToAdd = "fishing";
-        const response = getReply(taskToAdd);
-        User.todos.push(taskToAdd);
-        return `${taskToAdd} added to your todo`;
-      
+    //command format: "What is my name?"
+    if (command.match("What is my name?")) {
+        return `Your name is ${user.userName}`;
     }
+
+    //command format: Add task to my todo
+    const addPrefix = "Add ";
+    const addSuffix = " to my todo";
+    if (command.startsWith(addPrefix) && command.endsWith(addSuffix)) {
+        const task = command.slice(addPrefix.length, -addSuffix.length); // Extract the task dynamically
+        user.todos.push(task);
+        return `${task} added to your todo`;
+    }
+    // remove task to my todo
+    const removePrefix = "Remove ";
+    const removeSuffix = " from my todo";
+    if (command.startsWith(removePrefix) && command.endsWith(removeSuffix)) {
+        const task = command.slice(removePrefix.length, -removeSuffix.length); // Extract the task dynamically  
+        const index = user.todos.indexOf(task);
+        if (index !== -1) {
+            user.todos.splice(index, 1);
+            return `Removed ${task} from your todo`;
+        } else {
+            return `${task} was not found in your todo list`;
+        }
+    }
+
+
     return ("to be completed")
 }
-   
+// username 
 function extractName(sentence) {
     const nameStartIndex = "Hello my Name is ".length;
     const name = sentence.substring(nameStartIndex);
     return name;
-}       
+    // remove fishing
 
 
-console.log(getReply("Hello my Name is Hamadi"));
+
+}
+
+console.log(getReply("Hello my Name is jannah"));
 console.log(getReply("What is my name?"));
-console.log(response);
+console.log(getReply("Add fishing to my todo"));
+console.log(getReply("Add shopping to my todo"));
+console.log(getReply("Add singing in the shower to my todo"));
+console.log(getReply("Remove fishing from my todo"));
