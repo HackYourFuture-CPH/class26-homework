3bfcb5ae80ef0b4dc83268ffddb6c00ad0d6b3e7
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-09-23 23:18:57 +0100

    JS4

--- 3_javascript/javascript1/week4/script.js
+++ 3_javascript/javascript1/week4/script.js
@@ -38,8 +38,43 @@ function getReply(command) {
             return `${task} was not found in your todo list`;
         }
     }
+    // whats in my todo 
+    if (command.match("What is on my todo?")){
+        const numTodos = user.todos.length;
+        if (numTodos == 0) {
+            return "You have no todos.";
+          } else if (numTodos == 1) {
+            return "You have 1 todo: " + user.todos[0];
+          } else {
+            return `You have ${numTodos} todos: ${user.todos.join(', ')}`;
+          }
+    }
+    // what day is today 
+    if (command.match("What day is it today?")){
+        const today = new Date();
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+         return today.toLocaleDateString(undefined, options);
+    }
 
+    // what is 
+    const questionPrefix = "what is " 
+    if (command.startsWith(questionPrefix)){
+        
+        const question = command.slice(questionPrefix.length, command.length); // Extract the task dynamically
+        const result = eval(question);
+        if (isNaN(result)){
+            return "Invalid question"
+        } else {
+            return result
+        }
+    }
 
+    const timerPrefix = "Set a timer for ";
+    const timerSuffix = " minutes";
+    if (command.startsWith(timerPrefix) && command.endsWith(timerSuffix)) {
+        const numberOfMinutes = command.slice(timerPrefix.length, -timerSuffix.length); // Extract the minutes dynamically
+        setTimer(numberOfMinutes);
+    }
     return ("to be completed")
 }
 // username 
@@ -47,15 +82,24 @@ function extractName(sentence) {
     const nameStartIndex = "Hello my Name is ".length;
     const name = sentence.substring(nameStartIndex);
     return name;
-    // remove fishing
-
-
-
 }
 
+function setTimer(minutes) {
+    console.log(`Timer set for ${minutes} minute(s)`);
+  
+    setTimeout(function() {
+      console.log("Timer done");
+    }, minutes * 60 * 1000); // Convert minutes to milliseconds
+}
+
+  
 console.log(getReply("Hello my Name is jannah"));
 console.log(getReply("What is my name?"));
 console.log(getReply("Add fishing to my todo"));
 console.log(getReply("Add shopping to my todo"));
 console.log(getReply("Add singing in the shower to my todo"));
 console.log(getReply("Remove fishing from my todo"));
+console.log(getReply("What is on my todo?"));
+console.log(getReply("What day is it today?"));
+console.log(getReply("what is 3 + 3"));
+console.log(getReply("Set a timer for 1 minutes"));
