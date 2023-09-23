73eaa77bceb9c93cc264379f6b8106d074b0cfa1
Meraj Sharifi <merajsharifi@Merajs-MacBook-Pro.local>
2023-09-23 11:13:10 +0200

    edites are done

--- 3_javascript/javascript1/week4/JS1-W4.js
+++ 3_javascript/javascript1/week4/JS1-W4.js
@@ -1,37 +1,41 @@
 
-const user = {
-    userName: "Benjamin",
-    todos: [],
-}
-
+let userName = "";
+const todoList = []; 
+let timer;
 
 function getReply(command) {
-    if (command.match ("Hello my name is" && user.userName)){
-        return `Nice to meet you ${user.userName}!`;
-    } else if (command.match("Hello my name is")) {return "The name is not a match sorry!"};
-    
-    if (command.match ("What is my name?")){
-        return `Your name is ${user.userName}`;
-    }  
-    if (command.match (`Add fishing to my todo`)){
-        let todo = "fishing";
-        user.todos.push(todo);
-        return "fishing added to your todo";
-    } else if (command.match (`Add singing in the shower to my todo`)){
-        let todo = "singing";
-        user.todos.push(todo);
-        return "singing added to your todo";
-    } else if (command.match ("Remove fishing from my todo")){
-        let todo = "fishing";
-        user.todos.shift(todo)
-        return "Removed fishing from your todo"
-    } else if (command.match ("What is on my todo?")){
-        if (user.todos.length === 0){
-            return 'Your todo list is empty.'
-        } else {todoList = user.todos.join(' and ');
-        return `You have ${user.todos.length} todos: ${todoList}`}
-    } 
-    
+    if (command.startsWith("Hello my name is ")) {
+        const newName = command.replace("Hello my name is ", "");
+        if (userName) {
+            return `We already know each other. Hi again ${userName}.`;
+        } else {
+            userName = newName;
+            return `Nice to meet you ${userName}`;
+        }
+    } else if (command === "What is my name?") {
+        return userName ? `Your name is ${userName}` : "Your name is not set.";}
+
+    if (command.startsWith("Add ") && command.endsWith(" to my todo")) {
+        const task = command.replace("Add ", "").replace(" to my todo", "");
+        todoList.push(task); 
+        return `${task} added to your todo.`;
+    } else if (command.startsWith("Remove ") && command.endsWith(" from my todo")) {
+        const task = command.replace("Remove ", "").replace(" from my todo", "");
+        const index = todoList.indexOf(task); 
+        if (index !== -1) {
+            todoList.splice(index, 1); 
+            return `Removed ${task} from your todo.`;
+        } else {
+            return `${task} is not in your todo list.`;
+        }
+    } else if (command === "What is on my todo?") {
+        if (todoList.length === 0) {
+            return "You have no todos.";
+        } else {
+            const todoItems = todoList.join(", ");
+            return `You have ${todoList.length} todos - ${todoItems}`;
+        }}
+
     if (command.match ("What day is it today?")){
         const today = new Date();
         const options = { day: 'numeric', month: 'long', year: 'numeric' };
@@ -44,27 +48,29 @@ function getReply(command) {
             const result = eval(expression);
             return `The result is ${result}`;
         } catch (error) {
-            return 'I cannot perform that calculation.';
-        }
-    } 
-    if (command.startsWith('Set a timer for ')) {
-        const timeStr = command.substring(16); 
-        const timeInSeconds = parseInt(timeStr) * 60; 
-        
-        setTimeout(() => {
-            console.log('Timer done');
-        }, timeInSeconds * 1000);
-        
-        return `Timer set for ${timeStr} minutes.`;
-    } 
-
-    else {return 'I do not understand that command.';
+            return 'I cannot perform that calculation.';}
     }
+
+    if (command.startsWith("Set a timer for ")) {
+        const timeInMinutes = parseInt(command.match(/\d+/)[0]);
+        if (!isNaN(timeInMinutes)) {
+          const timeInMillis = timeInMinutes * 60 * 1000;
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            console.log("Timer done");
+          }, timeInMillis);
+          return `Timer set for ${timeInMinutes} minutes`;
+        } else {
+          return "Invalid timer format. Please specify the time in minutes.";}
+      } 
+      
+    else {return 'I do not understand that command.';}
 }
 
 
 console.log(getReply("Hello my name is Benjamin")); //Nice to meet you Benjamin! 
-console.log(getReply("Hello my name is Meraj")); //The name is not a match sorry!
+console.log(getReply("Hello my name is Benjamin")); //TWe already know each other. Hi again Benjamin.
+
 
 console.log(getReply("What is my name?")); // Your name is Benjamin
 
@@ -78,4 +84,7 @@ console.log(getReply("What day is it today?")); // September 21, 2023
 console.log(getReply('What is 3 + 3')); // The result is 6
 
 console.log(getReply('Set a timer for 4 minutes')); // Timer set for 4 minutes ///// Timer done
+
+console.log(getReply('hooo haa hhhaa hoo')); // I do not understand that command.
+
 
