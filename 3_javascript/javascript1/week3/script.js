56f0dc537efc92b10c3101a950c9adb177859454
Halima Hassan <hassanhalima5544@gmail.com>
2023-09-24 16:47:34 +0200

    when will we be there

--- 3_javascript/javascript1/week3/script.js
+++ 3_javascript/javascript1/week3/script.js
@@ -17,3 +17,28 @@ const names = [
   console.log(filteredNames); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
   
   
+  //When will we be there??
+  function calculateTravelTime(travelInformation) {
+    const { speed, destinationDistance } = travelInformation;
+  
+    
+    const totalTimeHours = destinationDistance / speed;
+  
+    
+    const hours = Math.floor(totalTimeHours);
+    const minutes = Math.round((totalTimeHours - hours) * 60);
+  
+    
+    const timeString = `${hours} hours and ${minutes} minutes`;
+  
+    return timeString;
+  }
+  
+  const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+  };
+  
+  const travelTime = calculateTravelTime(travelInformation);
+  console.log(travelTime); // Output: 8 hours and 38 minutes
+  
