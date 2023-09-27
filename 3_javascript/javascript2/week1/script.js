f396ae9f2305d4b2fb8b73c4eeaca30986ea8a58
shazkhan2 <seewardak@hotmail.com>
2023-09-27 12:12:06 +0200

    Final task completed

--- 3_javascript/javascript2/week1/script.js
+++ 3_javascript/javascript2/week1/script.js
@@ -28,4 +28,47 @@ let danishString2 = "Blå grød med røde bær";
 console.log(lettersCount(danishString2)); // Output: {å: 1, ø: 2, æ: 1, total: 4}
 
 // Create a page where a user writes his name in an input element. The user then clicks
-// a button. The user will now receive a spirit animal name, fx Benjamin - The fullmoon wolf.
+
+const button = document.getElementById("spirit");
+const newSpiritButton = document.getElementById("newSpirit");
+
+let userName = null;
+const spiritNames = [
+    "The fullmoon wolf", 
+    "The roaring lobster", 
+    "The killing octopus", 
+    "The predatory seagul",
+    "The majestic eagle",
+    "The ugly duckling",
+    "The spiralling gondalla",
+    "The plumpy cow",
+    "The conniving monkey",
+];
+
+button.addEventListener("click", generateName);
+newSpiritButton.addEventListener("click", generateSpiritAnimal);
+
+function generateName(event) {
+// What if the user clicks the generate new spirit animal and there is no text in the input?
+
+
+    if (!userName) {
+        userName = prompt("Please enter your name");
+        if (!userName) return;
+    }
+    const randomSpirits = Math.floor(Math.random() * spiritNames.length);
+    const randomName = spiritNames[randomSpirits];
+    
+    // Display the username and spirit animal
+    const spiritNameElement = document.getElementById("spiritName");
+    spiritNameElement.textContent = `Name: ${userName} - ${randomName}`;
+}
+
+function generateSpiritAnimal(event) {
+    const randomSpirits = Math.floor(Math.random() * spiritNames.length);
+    const randomName = spiritNames[randomSpirits];
+    
+    // Display only the spirit animal
+    const spiritNameElement = document.getElementById("spiritName");
+    spiritNameElement.textContent = `Name: ${randomName}`;
+}
