9e31851e1265e38510d75301b221e816e259b9d4
Nada Jrad <nadajrad@MacBook-Pro-de-Haytham.local>
2023-10-03 16:14:51 +0100

    complet but missing some part

---
+++ 3_javascript/javascript2/week1/main.js
@@ -0,0 +1,18 @@
+const products = getAvailableProducts();
+
+function renderProducts(products) {
+  const ul = document.querySelector('#productList'); // Select the <ul> element
+
+   for (const product of products) {
+
+    const li = document.createElement('li');
+
+      li.innerHTML = `<strong>Title:</strong> ${product.name}<br>
+      <strong>Price:</strong> $${product.price}<br>
+      <strong>Rating:</strong> ${product.rating}`;
+
+    ul.appendChild(li);
+  }
+}
+
+renderProducts(products);
--- 3_javascript/javascript2/week1/script.js
+++ 3_javascript/javascript2/week1/script.js
@@ -19,4 +19,38 @@ function countDanishLetters(inputString) {
 
   const danishString2 = "Blå grød med røde bær";
   console.log(countDanishLetters(danishString2));
-  
+   // spirit animal 
+   const spiritAnimals = [
+    "The Happy Bear",
+    "The Dancing Fox",
+    "The Curious Owl",
+    "The Soaring Hawk", 
+    "The Playful Dolphin", 
+    "The Wise Elephant", 
+    "The Loyal Dog", 
+    "The Graceful Swan", 
+    "The Energetic Cheetah", 
+    "The Mysterious Panther"
+];
+const nameInput = document.getElementById("nameInput");
+const generateBtn = document.getElementById("generateBtn");
+const getResult = document.getElementById("result");
+
+
+function getRandomSpiritAnimal() {
+    const randomIndex = Math.floor(Math.random() * spiritAnimals.length);
+    return spiritAnimals[randomIndex];
+}
+function generateSpiritAnimal() {
+    const userName = nameInput.value.trim();
+    if (userName === "") {
+        alert("Please enter your name.");
+        return;
+    }
+
+const spiritAnimal = getRandomSpiritAnimal();
+const result = `${userName} - ${spiritAnimal}`;
+getResult.textContent = result;
+}
+
+
