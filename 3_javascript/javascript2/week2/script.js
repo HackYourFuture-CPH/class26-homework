c76c97cbe91f9f19aed8b83c8a636a140fecbc27
270516 <ranjankomal93@gmail.com>
2023-10-04 13:11:46 +0200

    Completed the codewars katas

--- 3_javascript/javascript2/week2/script.js
+++ 3_javascript/javascript2/week2/script.js
@@ -13,3 +13,10 @@ console.log(doubleOfOddNumber);
 console.log("The doubled numbers are", newNumbers); // [2, 6]
 
 
+// Codewars links:
+// 8 kyu To square(root) or not to square(root)
+// https://www.codewars.com/kata/57f6ad55cca6e045d2000627/train/javascript
+// 8 kyu Removing Elements
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/javascript
+
+
