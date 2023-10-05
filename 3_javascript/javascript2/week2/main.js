fab024293ae6240c9e78f0b65737e46d0e822537
270516 <ranjankomal93@gmail.com>
2023-10-05 17:08:08 +0200

    Added 2.1, 2.2 & 2.3 tasks

--- 3_javascript/javascript2/week2/main.js
+++ 3_javascript/javascript2/week2/main.js
@@ -20,9 +20,34 @@ products.forEach(product => {
 });
 };
 
-
 renderProducts(products); 
 
-//2.1. Filter product
+//2.1. Filter product , 2.2 Searching for products, 2.3. Filter products based on max price
 
+function renderProducts(productArray) {
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = "";
 
+    if (productArray.length === 0) {
+      resultsDiv.textContent = "No matching products found.";
+    } else {
+      productArray.forEach((product) => {
+        const productDiv = document.createElement("div");
+        productDiv.textContent = `${product.name} = price: ${product.price}`;
+        resultsDiv.appendChild(productDiv);
+      });
+    }
+  }
+
+const searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener("click", function () {
+      const searchInput = document.getElementById("searchInput").value.toLowerCase();
+      const priceInput = parseFloat(document.getElementById("priceInput").value);
+
+      const filteredProducts = products.filter((product) => {
+        const productName = product.name.toLowerCase();
+        return productName.includes(searchInput) && (!priceInput || product.price <= priceInput);
+      });
+
+      renderProducts(filteredProducts);
+    });
