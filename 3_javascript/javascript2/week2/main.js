ae0869d592885924b4fb258407cb700c53f05983
Meraj Sharifi <merajsharifi@Merajs-MacBook-Pro.local>
2023-10-05 19:51:40 +0200

    homework part 2 is  done (hyfBay)

--- 3_javascript/javascript2/week2/main.js
+++ 3_javascript/javascript2/week2/main.js
@@ -1,10 +1,12 @@
-console.log("Script loaded");
+// console.log("Script loaded");
 
 const products = getAvailableProducts();
 
 function renderProducts(products) {
     const productList = document.querySelector("ul");
   
+    productList.innerHTML = '';
+
     products.forEach(product => {
       const listItem = document.createElement('li');
       listItem.innerHTML = 
@@ -17,4 +19,54 @@ function renderProducts(products) {
   }
   
 renderProducts(products);
+
+////////////////////////////////////////week 2////////////////////////////////////////
+////2.2 Searching for products
+
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('input', function () {
+  const searchQuery = searchInput.value.toLowerCase();
+
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchQuery)
+  );
+
+  renderProducts(filteredProducts);
+});
+
+
+
+////2.3. Filter products based on max price
+
+const maxPriceInput = document.getElementById('maxPriceInput');
+
+maxPriceInput.addEventListener('input', function () {
+  const maxPrice = parseFloat(maxPriceInput.value);
+
+  const filteredProducts = products.filter(product => product.price <= maxPrice);
+
+  renderProducts(filteredProducts);
+});
+
+
+////////2.4. Make the website look nicer!
+//CSS added
+
+
+////////2.5. Create some extra feature
+//Filter products based on rate (I know it is not so different, sorry, have not anything my mind)
+
+const highRate = document.getElementById('highRate');
+
+highRate.addEventListener('input', function () {
+  const maxRate = parseFloat(highRate.value);
+
+  const filteredProducts = products.filter(product => product.rating >= maxRate);
+
+  renderProducts(filteredProducts);
+});
+
+
+
 
