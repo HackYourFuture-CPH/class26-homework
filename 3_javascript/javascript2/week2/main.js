9a7a26181dba3a16cd62ab346d43d836df2baf20
Yağmur Meral <127700012+yagmureva@users.noreply.github.com>
2023-10-07 22:36:05 +0200

    changed becaue of review

--- 3_javascript/javascript2/week2/main.js
+++ 3_javascript/javascript2/week2/main.js
@@ -19,3 +19,29 @@ function renderProducts(products) {
 }
 
 renderProducts(products); 
+//2.2 Searching for products
+
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('input', function () {
+  const searchQuery = searchInput.value.toLowerCase();
+
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchQuery)
+  );
+
+  renderProducts(filteredProducts);
+});
+
+
+
+//2.3. Filter products based on max price
+
+const maxPriceInput = document.getElementById('maxPriceInput');
+
+maxPriceInput.addEventListener('input', function () {
+  const maxPrice = parseFloat(maxPriceInput.value);
+
+  const filteredProducts = products.filter(product => product.price <= maxPrice);
+
+  renderProducts(filteredProducts);)
