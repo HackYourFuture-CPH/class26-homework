6b77914e8d6d0e29486a878a21fa54c6eeb40c7e
Sevda Naziri <sevdanaziri@gmail.com>
2023-10-14 01:02:35 +0200

    made correctional changes

--- 3_javascript/javascript2/week2/Week2-HYF/main.js
+++ 3_javascript/javascript2/week2/Week2-HYF/main.js
@@ -1,41 +1,67 @@
 const products = getAvailableProducts();
+const productsUl = document.querySelector("section.products ul");
+const searchInput = document.getElementById("searchInput");
+const maxPriceInput = document.getElementById("maxPriceInput");
+const sortSelect = document.getElementById("sortSelect");
+const shipsToSelect = document.getElementById("shipsToSelect");
 
-function renderProducts(products) {
-  const ul = document.querySelector("#product-list");
-  products.forEach((product) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<strong>Title:</strong> ${product.title}<br>
-                    <strong>Price:</strong> $${product.price.toFixed(2)}<br>
-                    <strong>Rating:</strong> ${product.rating}`;
-    ul.appendChild(li);
-  });
-}
-
-//rendering of products
-renderProducts(products);
-
-// Filter by title
-const searchInput = document.querySelector(".search input");
 searchInput.addEventListener("input", () => {
-  const searchKeyword = searchInput.value.toLowerCase();
-  const filteredProducts = filterProducts(searchKeyword);
+  const searchText = searchInput.value.toLowerCase();
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchText)
+  );
   renderProducts(filteredProducts);
 });
 
-// Filter products based on max price
-const maxPriceInput = document.getElementById("start");
 maxPriceInput.addEventListener("input", () => {
   const maxPrice = parseFloat(maxPriceInput.value);
-  const filteredProducts = filterProductsByMaxPrice(maxPrice);
+  const filteredProducts = products.filter(
+    (product) => product.price <= maxPrice
+  );
   renderProducts(filteredProducts);
 });
 
-function filterProducts(keyword) {
-  return products.filter((product) => {
-    return product.title.toLowerCase().includes(keyword);
+sortSelect.addEventListener("change", () => {
+  const sortOption = sortSelect.value;
+
+  if (sortOption === "cheap") {
+    products.sort((a, b) => a.price - b.price);
+  } else if (sortOption === "expensive") {
+    products.sort((a, b) => b.price - a.price);
+  } else if (sortOption === "name") {
+    products.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  renderProducts(products);
+});
+
+shipsToSelect.addEventListener("change", () => {
+  const selectedCountry = shipsToSelect.value;
+  const filteredProducts = products.filter((product) =>
+    product.shipsTo.includes(selectedCountry)
+  );
+  renderProducts(filteredProducts);
+});
+
+function renderProducts(products) {
+  productsUl.innerHTML = "";
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    let shipsToHTML = "";
+    product.shipsTo.forEach(
+      (country) => (shipsToHTML += `<li>${country}</li>`)
+    );
+    li.innerHTML = `
+            <ul>
+                <li>${product.name}</li>
+                <li>${product.price} DKK</li>
+                <li>Rating: ${product.rating}</li>
+                <ul class="ships-to">${shipsToHTML}</ul>
+            </ul>
+        `;
+    productsUl.appendChild(li);
   });
 }
 
-function filterProductsByMaxPrice(maxPrice) {
-  return products.filter((product) => product.price <= maxPrice);
-}
+// Initial rendering of all products
+renderProducts(products);
