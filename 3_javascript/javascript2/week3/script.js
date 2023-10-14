06bf157214c058e8b986aaf4af4d5f0825b8aaaf
270516 <ranjankomal93@gmail.com>
2023-10-14 14:31:09 +0200

    completed the third exercise

--- 3_javascript/javascript2/week3/script.js
+++ 3_javascript/javascript2/week3/script.js
@@ -1,5 +1,5 @@
 console.log("It's homework time");
-
+// 1.( Warmup)
 // 1. & 2.
 domListener();
 
@@ -164,7 +164,51 @@ const obj = {valueFunction : "I love coding"}
 
 valueFunction(obj.valueFunction)
 
-//3. The fastest presser in this realm
+//3. The fastest presser in this realm (stuck in this)
+
+ domLoader()
+
+let sCounter = 0;
+let lCounter = 0;
+let gameDuration = 0;
+
+const durationInput = document.getElementById("durationInput");
+const startGameButton = document.getElementById("btn-3");
+const resultDiv = document.getElementById("result");
+
+        function startGame() {
+            const userInput = durationInput.value;
+            if (userInput) {
+                gameDuration = parseInt(userInput, 10);
+                resultDiv.innerHTML = `Game started! Type 'S' or 'L'.`;
+
+                setTimeout(() => {
+                    resultDiv.innerHTML = `Game over! You typed 'S' ${sCounter} times and 'L' ${lCounter} times.`; 
+                    determineWinner();
+                }, gameDuration * 1000);
+            }
+        }
+        startGameButton.addEventListener("click", startGame);
+
+        document.addEventListener("keydown", function (event) {
+            const key = event.key;
+            if (key === "S" || key === "s") {
+                sCounter++;
+            } else if (key === "L" || key === "l") {
+                lCounter++;
+            }
+        });
+
+        function determineWinner() {
+            if (sCounter > lCounter) {
+                resultDiv.innerHTML += "<br>S wins!";
+            } else if (lCounter > sCounter) {
+                resultDiv.innerHTML += "<br>L wins!";
+            } else {
+                resultDiv.innerHTML += "<br>It's a tie!";
+            }
+        }
+
 
 
 
