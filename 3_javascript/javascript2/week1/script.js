ce619d659dd6342a45421e95d885160aeba54468
Halima Hassan <hassanhalima5544@gmail.com>
2023-10-14 22:29:14 +0200

    changes added

--- 3_javascript/javascript2/week1/script.js
+++ 3_javascript/javascript2/week1/script.js
@@ -1,107 +1,65 @@
-//1.2. Find and count the Danish letters
-function countDanishLetters(inputString) {
-    const danishLetters = "æøå";
-    const letterCounts = {};
+// JavaScript
+document.getElementById('generateButton').addEventListener('click', function () {
+    const name = document.getElementById('nameInput').value;
+    // Call a function to generate and display the spirit animal name
+    generateSpiritAnimalName(name);
+});
 
-    for (let i = 0; i < inputString.length; i++) {
-        const char = inputString[i];
-        if (danishLetters.includes(char)) {
-            if (letterCounts[char]) {
-                letterCounts[char]++;
-            } else {
-                letterCounts[char] = 1;
-            }
-        }
-    }
+const spiritAnimals = [
+    "The Wise Owl",
+    "The Brave Lion",
+    "The Curious Fox",
+    "The Playful Dolphin",
+    "The Strong Bear",
+    "The Graceful Gazelle",
+    "The Gentle Panda",
+    "The Free Spirit Horse",
+    "The Mystical Wolf",
+    "The Colorful Peacock"
+];
 
-    let total = 0;
-    const result = { total };
-
-    for (const letter in letterCounts) {
-        total += letterCounts[letter];
-        result[letter] = letterCounts[letter];
-    }
-
-    return result;
-}
-
-const danishString = "Jeg har en blå bil";
-console.log(countDanishLetters(danishString));
-
-const danishString2 = "Blå grød med røde bær";
-console.log(countDanishLetters(danishString2));
-
-// 2. Spirit animal name generatorfind
-
-function generateSpiritAnimalName() {
-    const name = document.getElementById("nameInput").value;
-    const spiritAnimals = [
-        "The Fullmoon Wolf",
-        "The Wise Owl",
-        "The Mystic Panther",
-        "The Majestic Eagle",
-        "The Graceful Swan",
-        "The Fierce Tiger",
-        "The Playful Dolphin",
-        "The Gentle Gazelle",
-        "The Mysterious Fox",
-        "The Loyal Dog"
-    ];
+function generateSpiritAnimalName(name) {
     const randomSpiritAnimal = spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
     const spiritAnimalName = name + " - " + randomSpiritAnimal;
-
-    const spiritAnimalDiv = document.getElementById("spirit-animal");
-    spiritAnimalDiv.innerText = spiritAnimalName;
+    document.getElementById('spiritAnimalName').textContent = "Spirit Animal Name: " + spiritAnimalName;
 }
 
-document.getElementById("generateButton").addEventListener("click", generateSpiritAnimalName);
 
-// 2.2. Setting up the events
-
-
-
-const generateButton = document.getElementById("generateButton");
-const spiritAnimalDiv = document.getElementById("spirit-animal");
+document.getElementById('newAnimalButton').addEventListener('click', function () {
+    generateSpiritAnimalName(document.getElementById('nameInput').value);
+});
 
 
-function generateSpiritAnimalName() {
-    
-    const nameInput = document.getElementById("nameInput");
-    const name = nameInput.value;
-
-    
-    if (name.trim() === "") {
-        alert("Please enter your name before generating your spirit animal name.");
-        return;
+document.getElementById('generateButton').addEventListener('click', function () {
+    const name = document.getElementById('nameInput').value;
+    if (name) {
+        generateSpiritAnimalName(name);
+    } else {
+        alert("Please enter your name before generating a spirit animal.");
     }
+});
 
-    const spiritAnimals = [
-        "The Fullmoon Wolf",
-        "The Wise Owl",
-        "The Mystic Panther",
-        "The Majestic Eagle",
-        "The Graceful Swan",
-        "The Fierce Tiger",
-        "The Playful Dolphin",
-        "The Gentle Gazelle",
-        "The Mysterious Fox",
-        "The Loyal Dog"
-    ];
+// Event listeners for different options
+document.getElementById('generateButton').addEventListener('click', function () {
+    const name = document.getElementById('nameInput').value;
+    if (name) {
+        generateSpiritAnimalName(name);
+    }
+});
 
-    
-    const randomSpiritAnimal = spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
-    const spiritAnimalName = `${name} - ${randomSpiritAnimal}`;
+document.getElementById('nameInput').addEventListener('input', function () {
+    const name = document.getElementById('nameInput').value;
+    if (name) {
+        generateSpiritAnimalName(name);
+    }
+});
 
-    spiritAnimalDiv.innerText = `Name: ${spiritAnimalName}`;
-}
+document.getElementById('nameInput').addEventListener('mouseover', function () {
+    const name = document.getElementById('nameInput').value;
+    if (name) {
+        generateSpiritAnimalName(name);
+    }
+});
 
-
-generateButton.addEventListener("click", generateSpiritAnimalName);
-
-
-
-
-
-
-
+//
 
