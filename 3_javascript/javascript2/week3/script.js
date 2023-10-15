468336aafafe58f232ceb5bd597e878dc3d95270
Halima Hassan <hassanhalima5544@gmail.com>
2023-10-15 14:37:31 +0200

    function that calls out earth

--- 3_javascript/javascript2/week3/script.js
+++ 3_javascript/javascript2/week3/script.js
@@ -1,132 +1,131 @@
 //1.2 functions!
 
 function logAfterDelay(delay, stringToLog) {
-    setTimeout(function() {
-      console.log(stringToLog);
-    }, delay * 1000); 
-  }
-  
- 
-  logAfterDelay(2.5, "Called after 2.5 seconds");
-  logAfterDelay(5, "Called after 5 seconds");
-  logAfterDelay(1, "Called after 1 second");
-  
+    setTimeout(function () {
+        console.log(stringToLog);
+    }, delay * 1000);
+}
+
+
+logAfterDelay(2.5, "Called after 2.5 seconds");
+logAfterDelay(5, "Called after 5 seconds");
+logAfterDelay(1, "Called after 1 second");
+
 //
 function logAfterDelay(delay, stringToLog) {
-    setTimeout(function() {
-      console.log(stringToLog);
+    setTimeout(function () {
+        console.log(stringToLog);
     }, delay * 1000);
-  }
-  
-  var button = document.getElementById("logButton");
-  
-  button.addEventListener("click", function() {
+}
+
+var button = document.getElementById("logButton");
+
+button.addEventListener("click", function () {
     logAfterDelay(5, "Called after 5 seconds 5 seconds after the button is clicked");
-  });
+});
 
 //
 // Function to log "Earth"
 function logEarth() {
     console.log("Earth");
-  }
-  
+}
 
-  function logSaturn() {
+
+function logSaturn() {
     console.log("Saturn");
-  }
-  
-  // Function that calls the provided parameter function
-  function logPlanet(planetLogFunction) {
-    planetLogFunction();
-  }
-  
-  
-  logPlanet(logEarth);
-  
-  
-  logPlanet(logSaturn);
+}
 
-  
-  // Function to handle location logging
+// Function that calls the provided parameter function
+function logPlanet(planetLogFunction) {
+    planetLogFunction();
+}
+
+
+logPlanet(logEarth);
+
+
+logPlanet(logSaturn);
+
+
+// Function to handle location logging
 function logLocation() {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-      }, function(error) {
-        console.error("Error getting location: " + error.message);
-      });
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+        }, function (error) {
+            console.error("Error getting location: " + error.message);
+        });
     } else {
-      console.error("Geolocation is not supported by this browser.");
+        console.error("Geolocation is not supported by this browser.");
     }
-  }
-  
-  
-  var logButton = document.getElementById("logLocationButton");
-  
-  
-  logButton.addEventListener("click", logLocation);
+}
 
-  //
-  function runAfterDelay(delay, callback) {
-    setTimeout(callback, delay * 1000); 
-  }
-  
-  
-  function callback1() {
+
+var logButton = document.getElementById("logLocationButton");
+
+
+logButton.addEventListener("click", logLocation);
+
+//
+function runAfterDelay(delay, callback) {
+    setTimeout(callback, delay * 1000);
+}
+
+
+function callback1() {
     console.log("Callback 1 executed after delay");
-  }
-  
-  function callback2() {
+}
+
+function callback2() {
     console.log("Callback 2 executed after delay");
-  }
-  
-  
-  runAfterDelay(2, callback1);
-  runAfterDelay(4, callback2);
-  runAfterDelay(1, function() {
+}
+
+
+runAfterDelay(2, callback1);
+runAfterDelay(4, callback2);
+runAfterDelay(1, function () {
     console.log("Custom callback executed after 1-second delay");
-  });
+});
 //
 
 var clickCount = 0;
-var delay = 500; 
+var delay = 500;
 
-document.addEventListener("click", function() {
-  clickCount++;
+document.addEventListener("click", function () {
+    clickCount++;
 
-  if (clickCount === 1) {
-    setTimeout(function() {
-      if (clickCount === 2) {
-        console.log("Double click!");
-      }
-      clickCount = 0;
-    }, delay);
-  }
+    if (clickCount === 1) {
+        setTimeout(function () {
+            if (clickCount === 2) {
+                console.log("Double click!");
+            }
+            clickCount = 0;
+        }, delay);
+    }
 });
 //
 function jokeCreator(shouldTellFunnyJoke, logFunnyJoke, logBadJoke) {
     if (shouldTellFunnyJoke) {
-      logFunnyJoke();
+        logFunnyJoke();
     } else {
-      logBadJoke();
+        logBadJoke();
     }
-  }
-  
-  
-  function logFunnyJoke() {
+}
+
+
+function logFunnyJoke() {
     console.log("Why don't scientists trust atoms? Because they make up everything!");
-  }
-  
-  function logBadJoke() {
+}
+
+function logBadJoke() {
     console.log("Why did the scarecrow win an award? Because he was outstanding in his field!");
-  }
-  
-  
-  jokeCreator(true, logFunnyJoke, logBadJoke);
-  jokeCreator(false, logFunnyJoke, logBadJoke); 
-  
+}
 
 
-  
+jokeCreator(true, logFunnyJoke, logBadJoke);
+jokeCreator(false, logFunnyJoke, logBadJoke);
+
+
+
