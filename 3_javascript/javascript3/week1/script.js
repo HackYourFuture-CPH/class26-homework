3e88464239f9952dd649dd3dd931e13ed92ae6f0
Nada Jrad <nadajrad@MBP-de-Haytham.lan>
2023-10-19 23:56:56 +0200

    show image

--- 3_javascript/javascript3/week1/script.js
+++ 3_javascript/javascript3/week1/script.js
@@ -1,22 +1,31 @@
-// const url = 'https://website-screenshot6.p.rapidapi.com/screenshot?url=???????';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '3930b293d1msh8928109a5bc079fp13dbf3jsn32f25a4d9cc8',
-// 		'X-RapidAPI-Host': 'website-screenshot6.p.rapidapi.com'
-// 	}
-// };
+const url = 'https://website-screenshot6.p.rapidapi.com/screenshot?url=';
 
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
+document.getElementById("myBtn").addEventListener("click", showImage);
+function showImage (){
+    let imageUrl =  document.getElementById("url").value ;
+    let x = document.createElement("IMG");
+    x.setAttribute("src", imageUrl );
+    document.body.appendChild(x);
+    
 
-const fullName = 'Bonjour';
-document.getElementById("myBtn").addEventListener("click", greeting);
-function greeting (){
-console.log(fullName + document.getElementById("url").value) ;
+    let name = url + imageUrl ;
+    console.log(name);
+}
+function makeScreenShot (){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3930b293d1msh8928109a5bc079fp13dbf3jsn32f25a4d9cc8',
+            'X-RapidAPI-Host': 'website-screenshot6.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+    
 }
