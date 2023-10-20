1faef36b5cdc62f2b7a30ceb623c8315cb1e5071
270516 <ranjankomal93@gmail.com>
2023-10-20 16:07:49 +0200

    Finally done with giphy searcher

--- 3_javascript/javascript3/week1/Gif search/gifyapi.js
+++ 3_javascript/javascript3/week1/Gif search/gifyapi.js
@@ -1,9 +1,27 @@
+console.log("Let's create something interesting")
 
+function sendApiRequest() {
+   const userInput = document.getElementById("search-for-gif").value;
+   const numOfGifs = document.getElementById("num-of-gifs").value; // Get the number of GIFs
 
-function fetchGifApi (query) {
-   const gifApi = fetch("https://developers.giphy.com/docs/")
-   gifApi.then((response) =>  response.json())
-   .then((data) => {
-    console.log(data)
-   })
+   const apiKey = 'pYlUpqK0ZDhbIbyZSlLL92wXs7Ugmjfa';
+   const gifApiUrl = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${apiKey}&limit=${numOfGifs}`;
+
+   fetch(gifApiUrl)
+       .then((response) => response.json())
+       .then((json) => {
+           const resultContainer = document.getElementById("search-result");
+           resultContainer.innerHTML = ''; // Clear previous results
+
+           json.data.forEach((gifData) => {
+               const imgPath = gifData.images.fixed_height.url;
+               const img = document.createElement("img");
+               img.setAttribute("src", imgPath);
+               resultContainer.appendChild(img);
+           });
+       })
+       .catch((error) => {
+           console.error('Error:', error);
+       });
 }
+
