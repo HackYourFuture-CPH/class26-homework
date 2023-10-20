0c4d88ef8d8da66ea18333cd68cbef0b2456cfa6
Nada Jrad <nadajrad@MBP-de-Haytham.lan>
2023-10-20 17:07:07 +0200

    move files from the wrong folder

--- 3_javascript/javascript3/week3/script.js
+++ 3_javascript/javascript3/week3/script.js
@@ -1,16 +1,18 @@
-const url = 'https://website-screenshot6.p.rapidapi.com/screenshot?url=';
 
 document.getElementById("myBtn").addEventListener("click", showImage);
-function showImage (){
-    let imageUrl =  document.getElementById("url").value ;
-    let x = document.createElement("IMG");
-    x.setAttribute("src", imageUrl );
-    document.body.appendChild(x);
-    
 
-    let name = url + imageUrl ;
-    console.log(name);
+function prepareUrl () {
+    const url = 'https://website-screenshot6.p.rapidapi.com/screenshot?url=';
+    let imageUrl =  document.getElementById("url").value ;
+    return url + imageUrl + "&width=1920&height=1080&fullscreen=true"
 }
+
+function showImage (url){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", url );
+    document.body.appendChild(x);
+}
+
 function makeScreenShot (){
     const options = {
         method: 'GET',
@@ -20,12 +22,10 @@ function makeScreenShot (){
         }
     };
     
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
+    fetch(prepareUrl(), options)           //api for the get request
+    .then(response => response.json())
+    .then(data => {
+        showImage(data.scr)
+    });
     
 }
