5a29f712c10476741d3e5887844344db56486b69
Fetrat10 <128608996+Fetrat10@users.noreply.github.com>
2023-10-20 17:34:09 +0200

    my jason file

---
+++ 3_javascript/javascript3/week1/myjason.js
@@ -0,0 +1,55 @@
+//Create my own json file//  Football teams
+{
+    "football_teams" [
+      {
+        "team_name": "Manchester United",
+        "country": "England",
+        "founded": 1878,
+        "stadium": "Old Trafford",
+        "captain": "Harry Maguire",
+        "head_coach": "Ole Gunnar Solskjaer"
+      },
+      {
+        "team_name": "Real Madrid",
+        "country": "Spain",
+        "founded": 1902,
+        "stadium": "Santiago Bernabéu",
+        "captain": "Sergio Ramos",
+        "head_coach": "Carlo Ancelotti"
+      },
+      {
+        "team_name": "FC Bayern Munich",
+        "country": "Germany",
+        "founded": 1900,
+        "stadium": "Allianz Arena",
+        "captain": "Manuel Neuer",
+        "head_coach": "Julian Nagelsmann"
+      },
+      {
+        "team_name": "Paris Saint-Germain",
+        "country": "France",
+        "founded": 1970,
+        "stadium": "Parc des Princes",
+        "captain": "Marquinhos",
+        "head_coach": "Mauricio Pochettino"
+      },
+      {
+        "team_name": "Barcelona",
+        "country": "Spain",
+        "founded": 1899,
+        "stadium": "Camp Nou",
+        "captain": "Sergi Roberto",
+        "head_coach": "Xavi Hernandez"
+      }
+    ]
+  }
+
+  
+
+
+  
+
+
+
+
+  //Weather app//
