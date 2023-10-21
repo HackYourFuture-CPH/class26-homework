553eabb2418363abeb51e31077240d39418d1d87
Srushti Salke <srushtisalke380@gmail.com>
2023-10-22 01:08:43 +0200

    finished my javascript3 week1 homework

---
+++ 3_javascript/javascript3/week1/WeatherApp/weather_api.js
@@ -0,0 +1,12 @@
+const weather_api = "https://api.openweathermap.org/data/2.5/weather?q=copenhagen&appid=d343416b21414202682daff848a54ebe";
+
+async function getWeather() {
+    const response = await fetch(weather_api);
+    const data = await response.json();
+
+    
+    
+    console.log(data);
+}
+
+getWeather();
