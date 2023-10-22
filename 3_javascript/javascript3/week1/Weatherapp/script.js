dac348685847d555c25a2999e7c00bf26a2524fb
rumana-aafrin <rumanaraafi@gmail.com>
2023-10-22 08:48:00 +0200

    Implemented all the tasks

--- 3_javascript/javascript3/week1/Weatherapp/script.js
+++ 3_javascript/javascript3/week1/Weatherapp/script.js
@@ -1,3 +1,30 @@
+// Create your own json file
+fetch(`quotes.json`)
+.then(data => data.json())
+.then(json => console.log(json));
+
+
+// Find a cool api
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=copenhagen&appid=c61cb7dd3bb25c904233df02c8eb3c5a`;
+fetch(apiUrl)
+  .then((response) => {
+   if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+   
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data.name);
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
+
+
+
+//   Weather app
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
