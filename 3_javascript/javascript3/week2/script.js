6a33c1956b32b92e6d87be0d87d671e158713060
270516 <ranjankomal93@gmail.com>
2023-11-07 13:42:03 +0100

    resolved the reviews given by reviewer

--- 3_javascript/javascript3/week2/script.js
+++ 3_javascript/javascript3/week2/script.js
@@ -1,21 +1,42 @@
-console.log("Let's start craziness")
+console.log("Let's start craziness");
 
 const convert = document.getElementById("convert");
-      const result = document.getElementById("result");
-      const from = document.getElementById("from");
-      const to = document.getElementById("to");
-      const amount = document.getElementById("amount");
-      convert.addEventListener("click", function() {
-         let fromCurrency = from.value;
-         let toCurrency = to.value;
-         let amt = amount.value;
-         fetch(`https://open.er-api.com/v6/latest/USD${fromCurrency}`)
-         .then(response => {
-               return response.json();
-         })
-         .then(data => {
+const result = document.getElementById("result");
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const amount = document.getElementById("amount");
+
+function populateCurrencyOptions(selectElement, currencies) {
+    for (const currency in currencies) {
+        const option = document.createElement("option");
+        option.value = currency;
+        option.text = currency;
+        selectElement.appendChild(option);
+    }
+}
+
+fetch("https://open.er-api.com/v6/latest/USD")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const currencies = data.rates;
+        
+        populateCurrencyOptions(from, currencies);
+        populateCurrencyOptions(to, currencies);
+    });
+
+convert.addEventListener("click", function() {
+    let fromCurrency = from.value;
+    let toCurrency = to.value;
+    let amt = amount.value;
+    fetch(`https://open.er-api.com/v6/latest/USD${fromCurrency}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
             let rate = data.rates[toCurrency];
             let total = rate * amt;
             result.innerHTML = `${amt} ${fromCurrency} = ${total} ${toCurrency}`;
-         });
-      });
+        });
+});
