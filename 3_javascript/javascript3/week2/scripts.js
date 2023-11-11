93cc8efd445d79f0c3d30af15309d4105b5d8961
Aaron Moses <pastures77@hotmail.com>
2023-11-11 20:24:16 +0100

    modify currency conversion  and currency pairs from API feedback.

--- 3_javascript/javascript3/week2/scripts.js
+++ 3_javascript/javascript3/week2/scripts.js
@@ -1,38 +1,27 @@
-let fromCurrency = 'EUR';
-let toCurrency = 'DKK';
-let amount = 0;
-
 document.addEventListener('DOMContentLoaded', () => {
   const inputAmount = document.getElementById('amount');
   const fromCurrencySelect = document.getElementById('fromCurrency');
   const toCurrencySelect = document.getElementById('toCurrency');
   const clickConvert = document.getElementById('convert');
 
-  function populateCurrencyDropdowns() {
-    const currencyPairs = [
-      'USD',
-      'EUR',
-      'DKK',
-      'JPY',
-      'GBP',
-      'AUD',
-      'CAD',
-      'CHF',
-      'CNY',
-      'SEK',
-      'NZD',
-      'SGD',
-      'HKD',
-      'NOK',
-      'KRW',
-      'MXN',
-      'INR',
-      'BRL',
-      'ZAR',
-      'AED',
-      'RUB',
-    ];
+  let amount = 0; // Declare amount
 
+  function fetchCurrencyPairs() {
+    const apiKey = '325349696f77df328d988c2c';
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/codes`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const currencyPairs = Object.keys(data.supported_codes);
+        populateCurrencyDropdowns(currencyPairs);
+      })
+      .catch((error) => {
+        console.error('Error fetching currency pairs:', error);
+      });
+  }
+
+  function populateCurrencyDropdowns(currencyPairs) {
     currencyPairs.forEach((currency) => {
       const option = document.createElement('option');
       option.value = currency;
@@ -40,13 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
       fromCurrencySelect.appendChild(option);
       toCurrencySelect.appendChild(option.cloneNode(true));
     });
-
-    fromCurrencySelect.value = fromCurrency;
-    toCurrencySelect.value = toCurrency;
   }
 
   inputAmount.addEventListener('input', () => {
-    amount = parseFloat(inputAmount.value);
+    const amount = parseFloat(inputAmount.value); // Declare amount within the listener
   });
 
   function getData() {
@@ -61,6 +47,11 @@ document.addEventListener('DOMContentLoaded', () => {
       .then((data) => {
         console.log('Data received:', data);
         const conversionRate = data.conversion_rates[toCurrency];
+        if (conversionRate === undefined) {
+          throw new Error(
+            'Conversion rate not available for the selected currencies.'
+          );
+        }
         const convertedAmount = amount * conversionRate;
         displayConversionResult(conversionRate, convertedAmount);
       })
@@ -74,12 +65,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const printResult = document.getElementById('conversionResult');
     if (conversionRate === 'Error') {
       printResult.innerText = 'An error occurred while fetching data.';
+    } else if (conversionRate === undefined) {
+      printResult.innerText =
+        'Conversion rate not available for the selected currencies.';
     } else {
-      printResult.innerText = `Your conversion is: ${fromCurrency}:${amount} = ${toCurrency}:${convertedAmount}`;
+      printResult.innerText = `Your conversion is: ${fromCurrencySelect.value}:${amount} = ${toCurrencySelect.value}:${convertedAmount}`;
     }
   }
 
   clickConvert.addEventListener('click', getData);
 
-  populateCurrencyDropdowns();
+  fetchCurrencyPairs();
 });
