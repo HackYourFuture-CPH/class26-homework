4d2f320d7d1704200f4b301bf5a3add54eba6fbd
Shazkhan2 <seewardak@hotmail.com>
2023-11-14 12:23:54 +0100

    part 1 done

---
+++ 5_databases/week2/Shah.sql
@@ -0,0 +1,32 @@
+--1. Add a task  with these attributes: title, description, created, updated, due_date, status_id, user_id
+INSERT INTO task (title, description, created, updated, due_date, status_id, user_id)
+VALUES ('Be good', 'Stop beating your husband', 'created', 'updated', 'due_date', 'status_id', 'user_id');
+
+--2. Change the title of a task
+UPDATE task
+set title = "Sleep"
+where title = "Wash clothes";
+
+
+SELECT *
+from task;
+
+--3. Change a task due date
+UPDATE task
+SET due_date = 2017-10-15 13:05:09
+WHERE title = 'Wash clothes';
+
+--4. Change a task status
+UPDATE status
+SET name = 'Yet to begin'
+WHERE name = 'Not started';
+
+--5. Mark a task as complete
+UPDATE task
+SET status_id = 3
+WHERE title = 'Wash clothes';
+
+--6. Delete a task
+
+
+--7. In all the above queries, you choose which is the task that you will modify/delete.
