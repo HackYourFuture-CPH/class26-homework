029c7db6c709a0b71567f8d576518fefd648075c
Shazkhan2 <seewardak@hotmail.com>
2023-11-14 13:14:07 +0100

    task 2 half done

--- 5_databases/week2/Shah.sql
+++ 5_databases/week2/Shah.sql
@@ -30,3 +30,49 @@ WHERE title = 'Wash clothes';
 DELETE FROM task
 WHERE title = 'Wash clothes';
 
+/* Create a new database containing the following tables:
+Class: with the columns: id, name, begins (date), ends (date)
+Student: with the columns: id, name, email, phone, class_id (foreign key) */
+
+CREATE DATABASE Shah
+    DEFAULT CHARACTER SET = 'utf8mb4' SET DATEFORMAT "YYYY-MM-DD HH:MM:SS";
+    
+    USE Shah;
+
+CREATE TABLE `Student` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NULL,
+  `class_id` int(10) unsigned NULL,
+  PRIMARY KEY (`id`),
+    CONSTRAINT `fk_class` FOREIGN KEY (`class_id`) REFERENCES `status` (`id`) ON DELETE CASCADE,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `status` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `task` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text NULL DEFAULT NULL,
+  `created` DATETIME NOT NULL,
+  `updated` DATETIME NOT NULL,
+  `due_date` DATETIME NULL DEFAULT NULL,
+  `status_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_status` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+
+/*If you are done with the above tasks, you can continue with these advanced tasks:
+Create an index on the name column of the student table.
+Add a new column to the class table named status which can only have the following 
+values: not-started, ongoing, finished (hint: enumerations). */
