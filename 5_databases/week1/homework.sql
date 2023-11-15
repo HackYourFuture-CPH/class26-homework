d649e958377506ad2397d8543bd4f769b9be09b4
NadaJrad <nadajradbenaicha@gmail.com>
2023-11-15 21:37:55 +0100

    review back

--- 5_databases/week1/homework.sql
+++ 5_databases/week1/homework.sql
@@ -1,20 +1,53 @@
-SELECT COUNT(*) FROM task;
-SELECT COUNT(*) FROM task WHERE due_date IS NULL;
-SELECT COUNT(*) FROM task WHERE status_id = 3;
+--Q1
+SELECT COUNT(*) 
+FROM task;
 
-SELECT COUNT(*) FROM task WHERE status_id = 2 OR status_id = 1 ;
-SELECT * FROM task  ORDER BY created DESC; 
+--Q2
+SELECT COUNT(*) 
+FROM task 
+WHERE due_date IS NULL;
+
+--Q3
+SELECT COUNT(*) 
+FROM task 
+WHERE status_id = 3;
+
+--Q4
+SELECT COUNT(*) 
+FROM task 
+WHERE status_id != 3;
+--Q5
+SELECT * 
+FROM task  
+ORDER BY created DESC; 
+
+--Q6
 SELECT *
-FROM task ORDER BY created DESC
+FROM task 
+ORDER BY created DESC
 LIMIT 1;
-SELECT title, due_date FROM task WHERE title LIKE "%database%" OR description  LIKE "%database%"  ;
-SELECT  task.title,status.name FROM task JOIN status ;
+
+--Q7
+SELECT title, due_date 
+FROM task 
+WHERE title LIKE "%database%" 
+OR description  LIKE "%database%";
+
+--Q8
+SELECT task.title,status.name 
+FROM task 
+JOIN status 
+ON task.status_id = status.id;
+
+--Q9
 SELECT status.name, COUNT(task.id) FROM status
- JOIN task ON  status_id = task.status_id
+JOIN task ON status.id = task.status_id
 GROUP BY status.name ;
 
-SELECT status.name, COUNT(*) AS status_count
+--Q10
+SELECT status.name, COUNT(task.id) AS status_count
 FROM status
 JOIN task ON task.status_id = status.id
 GROUP BY status.name 
 ORDER BY status_count DESC;
+
