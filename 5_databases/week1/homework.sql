b482d95344c30891ca6b1b2128b84c19f25b5722
Meraj Sharifi <merajsharifi@Merajs-MacBook-Pro.local>
2023-11-16 09:55:10 +0100

    edits are done

--- 5_databases/week1/homework.sql
+++ 5_databases/week1/homework.sql
@@ -2,7 +2,7 @@ USE hyf_lesson1;
 
 -- 1 Find out how many tasks are in the task table
 
-SELECT COUNT (title) FROM task 
+SELECT COUNT (*) FROM task 
 
 -- 2 Find out how many tasks in the task table do not have a valid due date
 
@@ -18,19 +18,25 @@ SELECT * FROM task WHERE status_id != 3
 
 -- 5 Get all the tasks, sorted with the most recently created first
 
-SELECT * FROM task ORDER BY created 
+SELECT * FROM task ORDER BY created DESC 
 
 -- 6 Get the single most recently created task
 
-SELECT * FROM task ORDER BY created LIMIT 1 
+SELECT * FROM task ORDER BY created DESC LIMIT 1 
 
 -- 7 Get the title and due date of all tasks where the title or description contains database
 
-SELECT title, due_date FROM task WHERE title LIKE "%database%" 
+SELECT title, due_date
+FROM task
+WHERE
+    title LIKE "%database%"
+    OR description LIKE "%database%"
 
 -- 8 Get the title and status (as text) of all tasks
 
-SELECT task.title, status.name FROM task JOIN status 
+SELECT task.title, status.name
+FROM task
+    JOIN status ON status.id = task.status_id
 
 -- 9 Get the name of each status, along with a count of how many tasks have that status
 
