cee2838375559b312c0b174bd02ad24ff706e84e
Nicolas Quiroga <akebra77@gmail.com>
2023-12-30 14:43:30 +0100

    he suggestions from the review were applied.

--- 6_nodejs/week1/avg.js
+++ 6_nodejs/week1/avg.js
@@ -1,14 +1,24 @@
-
-const args = require('minimist')(process.argv.slice(2));
+const args = require("minimist")(process.argv.slice(2));
 
 console.log(args.a);
 console.log(args.b);
 console.log(args.c);
 
-const Number1 = args.a
-const Number2 = args.b
-const Number3 = args.c
+const Number1 = args.a;
+const Number2 = args.b;
+const Number3 = args.c;
 
-let result = (Number1 + Number2 + Number3) / 3;
-
-console.log(result)
+if (Number1.trim() === "" || Number2.trim() === "" || Number3.trim() === "") {
+  console.log("Please check the empty spaces");
+} else {
+  if (
+    !isNaN(Number(Number1)) &&
+    !isNaN(Number(Number2)) &&
+    !isNaN(Number(Number3))
+  ) {
+    let result = (Number(Number1) + Number(Number2) + Number(Number3)) / 3;
+    console.log(result);
+  } else {
+    console.log("Invalid argument");
+  }
+}
