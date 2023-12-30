e3bd10300ffbe95b64da8d9947928ff253f7c806
Nicolas Quiroga <akebra77@gmail.com>
2023-12-30 15:10:36 +0100

    he suggestions from the review were applied.

--- 5_databases/week3/Homework.sql
+++ 5_databases/week3/Homework.sql
@@ -29,7 +29,7 @@ CREATE TABLE
         `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
         `title` VARCHAR(255) NOT NULL,
         `description` TEXT,
-        `meal_id` INT(10),
+        `meal_id` INT(10) NOT NULL,
         `stars` INT(10), 
         `created_date` DATE
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
@@ -57,8 +57,8 @@ WHERE id = 1;
 SELECT * 
 FROM `Reservation`;
 
-INSERT INTO Reservation (id,number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
-VALUES(1, '2', '2024-06-01 20:00:00', '+54 1812 2022', 'Lionel', 'LioM10@gmail.com', '3');
+INSERT INTO Reservation (number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
+VALUES( '2', '2024-06-01 20:00:00', '+54 1812 2022', 'Lionel', 'LioM10@gmail.com', '3');
 
 SELECT * 
 FROM `Reservation`
@@ -76,8 +76,8 @@ WHERE id=1;
 SELECT * 
 FROM `Review`;
 
-INSERT INTO `Review` (id,title, description, meal_id, stars, created_date)
-VALUES(1, 'Milanesas', 'A delightful blend of perfectly seasoned meat and a golden, crispy coating, delivering a tantalizing taste experience', 1, 4, '2023-01-29 11:00:00');
+INSERT INTO `Review` (title, description, meal_id, stars, created_date)
+VALUES('Milanesas', 'A delightful blend of perfectly seasoned meat and a golden, crispy coating, delivering a tantalizing taste experience', 1, 4, '2023-01-29 11:00:00');
 
 SELECT * 
 FROM `Review`
@@ -102,24 +102,24 @@ INSERT INTO Meal (title, description, location, `when`, max_reservations, price,
 VALUES ('Stegt flæsk med persillesovs', 'Fried pork belly with parsley sauce, served with boiled potatoes.', 'Roskilde', '2023-05-25 12:00:00', 8, 15.99, '2023-01-15');
 
 -- reservations
-INSERT INTO Reservation (id,number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
-VALUES(1, '1', '2024-06-01 20:00:00', '+54 1812 2022', 'Lionel', 'LioM10@gmail.com', '1');
+INSERT INTO Reservation (number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
+VALUES( '1', '2024-06-01 20:00:00', '+54 1812 2022', 'Lionel', 'LioM10@gmail.com', '1');
 
-INSERT INTO Reservation (id,number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
-VALUES(2, '7', '2024-01-29 20:30:00', '+966 7007 0770', 'Cristiano', 'CR7@gmail.com', '2');
+INSERT INTO Reservation (number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
+VALUES('7', '2024-01-29 20:30:00', '+966 7007 0770', 'Cristiano', 'CR7@gmail.com', '2');
 
-INSERT INTO Reservation (id,number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
-VALUES(3, '9', '2024-06-01 20:00:00', '+47 2023 0009', 'Erling', 'Viking@gmail.com', '3');
+INSERT INTO Reservation (number_of_guests, created_date, contact_phonenumber, contact_name, contact_email, meal_id)
+VALUES( '9', '2024-06-01 20:00:00', '+47 2023 0009', 'Erling', 'Viking@gmail.com', '3');
 
 -- review
-INSERT INTO `Review` (id,title, description, meal_id, stars, created_date)
-VALUES(1, 'Milanesas', 'A delightful blend of perfectly seasoned meat and a golden, crispy coating, delivering a tantalizing taste experience', 1, 5, '2023-01-29 11:00:00');
+INSERT INTO `Review` (title, description, meal_id, stars, created_date)
+VALUES( 'Milanesas', 'A delightful blend of perfectly seasoned meat and a golden, crispy coating, delivering a tantalizing taste experience', 1, 5, '2023-01-29 11:00:00');
 
-INSERT INTO `Review` (id,title, description, meal_id, stars, created_date)
-VALUES(2, 'Empanadas', 'These empanadas boasted a delightful fusion of flavors within their golden, crispy pastry, a true culinary delight.', 2, 4, '2023-05-10 14:00:00');
+INSERT INTO `Review` (title, description, meal_id, stars, created_date)
+VALUES( 'Empanadas', 'These empanadas boasted a delightful fusion of flavors within their golden, crispy pastry, a true culinary delight.', 2, 4, '2023-05-10 14:00:00');
 
-INSERT INTO `Review` (id,title, description, meal_id, stars, created_date)
-VALUES(3, 'Stegt flæsk med persillesovs', 'Crispy, perfectly paired with creamy parsley sauce, a quintessential Danish comfort dish', 3, 3, '2023-06-06 18:00:00');
+INSERT INTO `Review` (title, description, meal_id, stars, created_date)
+VALUES('Stegt flæsk med persillesovs', 'Crispy, perfectly paired with creamy parsley sauce, a quintessential Danish comfort dish', 3, 3, '2023-06-06 18:00:00');
 
 
 -- Get meals that has a price smaller than a specific price fx 90
@@ -135,6 +135,12 @@ JOIN Reservation ON Meal.id = Reservation.meal_id
 GROUP BY Meal.id
 HAVING Meal.max_reservations > COUNT(Reservation.id);
 
+SELECT meal.id, meal.title, meal.max_reservations, SUM(reservation.number_of_guests) AS total_reservations
+FROM meal
+LEFT JOIN Reservation ON meal.id = Reservation.meal_id
+GROUP BY meal.id
+HAVING COALESCE(total_reservations, 0) < meal.max_reservations;
+
 -- Get meals that partially match a title. Rød grød med will match the meal with the title Rød grød med fløde
 SELECT *
 FROM Meal
@@ -143,10 +149,8 @@ WHERE title LIKE "%persille%" ;
 
 -- Get meals that has been created between two dates
 SELECT *
-FROM Meal
-WHERE  Meal.created_date >= '2020-01-01'
-AND Meal.created_date <= '2023-11-05';
-
+FROM meal
+WHERE created_date BETWEEN '2023-10-01' AND '2023-10-31';
 
 -- Get only specific number of meals fx return only 2 meals
 SELECT * 
@@ -168,10 +172,23 @@ JOIN Reservation ON Meal.id = Reservation.meal_id
 WHERE Meal.title LIKE "Mila%"
 ORDER BY Reservation.created_date ASC;
 
+SELECT Reservation.*
+FROM Meal
+JOIN Reservation ON Meal.id = Reservation.meal_id
+WHERE Meal.title LIKE 'Mila%'
+ORDER BY Reservation.created_date ASC;
+
+
 
 -- Sort all meals by average number of stars in the reviews
 SELECT Meal.title, AVG(Review.stars)
 FROM Meal
 JOIN Review ON Meal.id = Review.meal_id
+GROUP BY Meal.title
+ORDER BY AVG(Review.stars) DESC;
+
+SELECT Meal.title, AVG(Review.stars) 
+FROM Meal
+LEFT JOIN Review ON Meal.id = Review.meal_id
 GROUP BY Meal.title
 ORDER BY AVG(Review.stars) DESC;
