4a9743c4c35420b11432e8460d2ce99b1d5e8752
shazkhan2 <115549210+shazkhan2@users.noreply.github.com>
2024-01-01 15:13:28 +0000

    Recommended changes done

--- 5_databases/week2/task1_2.sql
+++ 5_databases/week2/task1_2.sql
@@ -7,7 +7,7 @@ VALUES ('Be good', 'Stop beating your husband', '2023-11-16 12:00:00', '2023-11-
 --2. Change the title of a task
 UPDATE task
 set title = "Sleep"
-where title = "Wash clothes";
+where id = 4;
 
 
 SELECT *
@@ -15,16 +15,13 @@ from task;
 
 --3. Change a task due date
 UPDATE task
-SET due_date = 2017-10-15 
+SET due_date = '2017-10-15'  
 WHERE title = 'Wash clothes';
 
 --4. Change a task status
-UPDATE status
-SET name = 'Yet to begin'
-WHERE name = 'Not started';
-
-SELECT *
-from status;
+UPDATE task
+SET title = 'feed the dog'
+WHERE status_id = 1;
 
 --5. Mark a task as complete
 UPDATE task
@@ -54,18 +51,17 @@ CREATE TABLE `class` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `student` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY,  
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NULL,
   `class_id` int(10) unsigned NULL, BIGINT
-  PRIMARY KEY (`id`),
     CONSTRAINT `fk_class` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE CASCADE,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 /*Create an index on the name column of the student table.*/
-CREATE INDEX idx_name student (name);
+CREATE INDEX idx_name ON student (name);
 
 /*Add a new column to the class table named status which can only have the following 
 values: not-started, ongoing, finished (hint: enumerations). */
