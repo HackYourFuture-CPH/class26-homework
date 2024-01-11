44e9e0b948b06836af1685d1ff364ca0fc6928ba
rumana-aafrin <rumanaraafi@gmail.com>
2024-01-11 15:05:24 +0100

    Updated

--- 7_react/react1/week1/src/App.js
+++ 7_react/react1/week1/src/App.js
@@ -5,9 +5,9 @@ function MyLists(props){
     const description= props.description;
     const deadline= props.deadline;
   return(
-    <ul>
-    <li>{description}, {deadline}</li>
-    </ul>
+    <li>
+      {description}, {deadline}
+    </li>
   );
 }
 
@@ -15,9 +15,11 @@ function App() {
   return (
         <div className= "App">
           <h3>Todo List</h3>
+          <ul>
           <MyLists description= "Get out of bed" deadline= "Wed Sep 13 2017"/>
           <MyLists description= "Brush Teeth" deadline= "Thu Sep 14 2017"/>
           <MyLists description= "Eat Breakfast" deadline= "Fri Sep 15 2017"/>
+          </ul>
           </div>
   );
 }
