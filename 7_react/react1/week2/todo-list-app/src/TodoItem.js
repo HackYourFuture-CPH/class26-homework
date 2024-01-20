bd905fb60a67a6afc16d528cb7b859fb55433538
Meraj Sharifi <merajsharifi@Merajs-MacBook-Pro.local>
2024-01-20 11:10:07 +0100

    done

--- 7_react/react1/week2/todo-list-app/src/TodoItem.js
+++ 7_react/react1/week2/todo-list-app/src/TodoItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const TodoItem = (props) => {
- 
+const TodoItem = ({ id, description, deadline, done, toggleDone, deleteTask }) => {
   return (
-    <div id="items">
-      <input type="checkbox" /> 
-      {props.description}, {props.deadline}
+    <div style={{ textDecoration: done ? 'line-through' : 'none' }}>
+      <input type="checkbox" checked={done} onChange={toggleDone} />
+      <span>{description} {deadline}</span>
+      <button onClick={deleteTask}>Delete</button>
     </div>
   );
 };
--- 7_react/react1/week2/todo-list-app/src/TodoList.js
+++ 7_react/react1/week2/todo-list-app/src/TodoList.js
@@ -2,45 +2,64 @@ import React, { useState } from 'react';
 import TodoHeader from './TodoHeader';
 import TodoItem from './TodoItem';
 
-const intialList = [{
-  num: 1,
-  description: "Get out of bed",
-  deadline: "Wed Sep 13 2017"
-},
-{
-  num: 2,
-  description: "Brush teeth",
-  deadline: "Thu Sep 14 2017"
-},
-{
-  num: 3,
-  description: "Eat breakfast",
-  deadline: "Fri Sep 15 2017"
-}]
+const todos = [
+  {
+    id: 1,
+    description: "Get out of bed",
+    done: false,
+  },
+  {
+    id: 2,
+    description: "Brush teeth",
+    done: false,
+  },
+  {
+    id: 3,
+    description: "Eat breakfast",
+    done: false,
+  },
+];
 
 const TodoList = () => {
-   
-  const [todoList, setList] = useState(intialList)
-  const todoListItem = todoList.map(item => (
-    <TodoItem 
-    key={item.num} 
-    description={item.description} 
-    deadline={item.deadline}/>))
-    
+  const [todoList, setList] = useState(todos);
+
+  const toggleDone = (id) => {
+    setList((prevList) =>
+      prevList.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
+    setList((prevList) => prevList.filter((item) => item.id !== id));
+  };
+
+  const todoListItem = todoList.map((item) => (
+    <TodoItem
+      key={item.id}
+      description={item.description}
+      done={item.done}
+      toggleDone={() => toggleDone(item.id)}
+      deleteTask={() => deleteTask(item.id)}
+    />
+  ));
+
   const addingTask = () => {
     const newTask = {
-      num: 3,
-      description: "new task",
-      deadline: "now"
-    }
-    setList((previousState) => [...previousState, newTask])
-  }
+      id: todoList.length + 1,
+      description: "New Task",
+      done: false,
+    };
+    setList((prevList) => [...prevList, newTask]);
+  };
 
   return (
     <div>
       <TodoHeader />
       {todoListItem}
-      <button onClick={addingTask}>add a task</button>
+      {todoList.length === 0 && <p>No items...</p>}
+      <button onClick={addingTask}>Add a task</button>
     </div>
   );
 };
