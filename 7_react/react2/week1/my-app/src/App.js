1a6cd12dff0292d9d25434db52e712bdad8b252c
shazkhan2 <seewardak@hotmail.com>
2024-02-02 18:09:20 +0100

    context added

--- 7_react/react2/week1/my-app/src/App.js
+++ 7_react/react2/week1/my-app/src/App.js
@@ -1,44 +1,20 @@
-import React, { useState, useEffect } from 'react';
+// App.js
+import React from 'react';
 import './App.css';
+import { useAppContext } from './Context/SearchContext';
+import InPut from './Components/InPut';
+import List from './Components/List';
+import Loading from './Components/Loading';
+
+
 
 function App() {
-  const [query, setQuery] = useState('');
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await fetch(`https://api.github.com/search/users?q=${query}`);
-        const result = await res.json();
-        const users = result.items || [];
-        setData(users);
-      } catch (error) {
-        console.error('Error fetching users:', error.message);
-      }
-    };
-
-    if (query.length === 0 || query.length > 1) {
-      fetchUsers();
-    }
-  }, [query]);
-
   return (
     <div className="App">
       <h1>GitHub User Searcher</h1>
-      <input
-        placeholder="Search..."
-        className="search"
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      {data.length > 0 ? (
-        <div>
-          {data.map((user) => (
-            <div key={user.id}>{user.login}</div>
-          ))}
-        </div>
-      ) : (
-        <p>No results...</p>
-      )}
+      <InPut />
+      <Loading />
+      <List />
     </div>
   );
 }
