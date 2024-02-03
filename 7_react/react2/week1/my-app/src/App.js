3abb61ec56667642301014d3a8c5ce1f6217762e
Aaron Moses <pastures77@hotmail.com>
2024-02-03 20:52:42 +0100

    React 2 week 1 hw added.

---
+++ 7_react/react2/week1/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Search from './Search';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-header">GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+};
+
+export default App;
