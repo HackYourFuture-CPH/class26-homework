86a51bafdd4ffec831c0956442178661e27866d8
BJORN-VINTHER <BJORN.VINTHER@MAERSK.COM>
2024-02-08 18:16:03 +0100

    fixed issues in talent garden

--- extra-session/clean-code/src/App.js
+++ extra-session/clean-code/src/App.js
@@ -1,38 +1,81 @@
+function App() {
+  const la = [
+    "You should name your variables and functions in a way that makes it clear what they do.",
+    "You should use functions to avoid repeating code.",
+    "You should make your functions do one thing and do it well.",
+    "You should make sure to use the right data types for your variables.",
+    "You should indent your code properly.",
+    "You should organize your files and folders in a way that makes it easy to find things.",
+    "You should write your styles in css files and not in the html file.",
+  ];
 
+  const check = ["Yes", "No", "No", "Yes", "No", "No"];
 
-  function App() {
-const la = [
-  "You should name your variables and functions in a way that makes it clear what they do.",
-  "You should use functions to avoid repeating code.",
-  "You should make your functions do one thing and do it well.",
-  "You should make sure to use the right data types for your variables.", "You should indent your code properly.",
-  "You should organize your files and folders in a way that makes it easy to find things.",
-  "You should write your styles in css files and not in the html file.",
-;
-const check = [ "Yes",  "No", "No", "Yes",
-  "No", "No",
-]
-const doStuff =  { 
-  console.log('stuff');
-  la[0] = "[✓] " + la[0];
-  check[0] = "Yes"; }
+  
 
-      const doStuff2 = () => {
-            console.log('stuff2');
-        la[0] = "[x] " +       la[0];
-        check[0] = "No";
-      }
+  return (
+    <>
+      <h1>This is how you read clean code:</h1>
+      <HintItemList />
+      {/* <ol>
+        {" "}
+        <li class="green">
+          [✓] {la[0]} <button click={doStuff2()}>Stuff 2</button>
+        </li>
+        <li style={{ color: "red" }}>
+          [x] la[1]
+          <button click={doStuff()}>Yes</button>
+        </li>
+        <li style={{ color: "red" }}>
+          [x] la[2]<button click={doStuff2()}></button>
+        </li>{" "}
+        <li class="greeen">
+          [✓] la[3]<button click={doStuff2()}>Stuff 2</button>
+        </li>
+        <li style={{ color: "red" }}>
+          [x] la[2]<button click={doStuff()}>Yes</button>
+        </li>
+        <li style={{ color: "red" }}>
+          [x] la[3]<button click={doStuff2()}>Yes</button>
+        </li>
+      </ol> */}
+      {/* <button click={doStuff()}></button> */}
+    </>
+  );
+}
 
-return <>
-<h1>This is how you read clean code:</h1>
-    <ol> <li class="green">[✓] la[0]<button click={doStuff2()}>Stuff 2</button></li>
-      <li style={{color: "red"}}>[x] la[1]<button click={doStuff()}>Yes</button></li>
-<li style={{color: "red"}}>[x] la[2]<button click={doStuff2()}></button></li> <li class="greeen">[✓] la[3]<button click={doStuff2()}>Stuff 2</button></li>
-          <li style={{color: "red"}}>[x] la[2]<button click={doStuff()}>Yes</button></li>
-      <li style={{color: "red"}}>[x] la[3]<button click={doStuff2()}>Yes</button></li>
+function HintItemList() {
+  return (
+    <ol>
+      <HintItem text="You should name your variables and functions in a way that makes it clear what they do." checked={true} />
+      <HintItem text="You should use functions to avoid repeating code." checked={false} />
     </ol>
-    <button click={doStuff()}>></button>
-  </>
+  );
+}
+
+function HintItem(props) {
+
+  const doStuff = () => {
+    // console.log("stuff");
+    // la[0] = "[✓] " + la[0];
+    // check[0] = "Yes";
+  };
+
+  const doStuff2 = () => {
+    // console.log("stuff2");
+    // la[0] = "[x] " + la[0];
+    // check[0] = "No";
+  };
+
+  return (
+    <li class="greeen">
+      <input type="checkbox" checked={props.checked} />
+      [x] [✓]
+      {props.text}
+      <button click={() => doStuff()}>Yes</button>
+      {/* [✓] <p>la[3]</p><button click={doStuff2()}>Stuff 2</button> */}
+    </li>
+  );
 }
 
 export default App;
